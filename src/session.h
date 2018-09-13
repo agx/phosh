279e63a0a9431b4ae3c77a0361fb4cf3213c3f21
Guido Günther <agx@sigxcpu.org>
2018-09-13 16:32:57 +0200

    Register phosh with gnome-session
    Closes: #44
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

---
+++ src/session.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2018 Purism SPC
+ * SPDX-License-Identifier: GPL-3.0+
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#pragma once
+
+void phosh_session_register (const char *client_id);
+void phosh_session_unregister (void);
