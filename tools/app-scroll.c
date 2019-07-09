58a8a756fa235c39b133a3dc8ce81ff18a2ed07c
Zander Brown <zbrown@gnome.org>
2019-07-09 13:54:25 +0100

    tools: app-scroll demo
    Simple demo of PhoshApp

---
+++ tools/app-scroll.c
@@ -0,0 +1,89 @@
+
+/*
+ * Copyright (C) 2019 Zander Brown
+ * SPDX-License-Identifier: GPL-3.0+
+ * Author: Zander Brown <zbrown@gnome.org>
+ */
+
+#include <gtk/gtk.h>
+#include <app.h>
+
+int
+main (int argc, char **argv)
+{
+  GtkWidget *window = NULL;
+  GtkWidget *scrolled = NULL;
+  GtkWidget *box = NULL;
+  GtkWidget *app = NULL;
+  GtkCssProvider *provider = NULL;
+  GFile *file = NULL;
+  GError *error = NULL;
+
+  gtk_init (&argc, &argv);
+
+  provider = gtk_css_provider_new ();
+  file = g_file_new_for_uri ("resource:///sm/puri/phosh/style.css");
+
+  if (!gtk_css_provider_load_from_file (provider, file, &error)) {
+    g_warning ("Failed to load CSS file: %s", error->message);
+    g_clear_error (&error);
+    return 1;
+  }
+  gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
+                                             GTK_STYLE_PROVIDER (provider), 600);
+
+
+  window = g_object_new (GTK_TYPE_WINDOW,
+                         "visible", TRUE,
+                         "default-height", 640,
+                         "default-width", 360,
+                         "height-request", 640,
+                         "width-request", 360,
+                         "title", "PhoshApp Demo",
+                         NULL);
+  g_signal_connect (window, "delete-event", G_CALLBACK (gtk_main_quit), NULL);
+
+  scrolled = g_object_new (GTK_TYPE_SCROLLED_WINDOW,
+                           "vscrollbar-policy", GTK_POLICY_NEVER,
+                           "visible", TRUE,
+                           NULL);
+  gtk_container_add (GTK_CONTAINER (window), scrolled);
+
+  box = g_object_new (GTK_TYPE_BOX,
+                      "spacing", 18,
+                      "margin-start", 24,
+                      "margin-end", 24,
+                      "margin-top", 10,
+                      "margin-bottom", 10,
+                      "halign", GTK_ALIGN_CENTER,
+                      "valign", GTK_ALIGN_CENTER,
+                      "visible", TRUE,
+                      NULL);
+  gtk_container_add (GTK_CONTAINER (scrolled), box);
+
+  app = g_object_new (PHOSH_TYPE_APP,
+                      "app-id", "org.gnome.Calculator",
+                      "title", "1 + 1 = 2",
+                      "win-width", 360,
+                      "win-height", 640,
+                      "max-width", 240,
+                      "max-height", 400,
+                      "visible", TRUE,
+                      NULL);
+  gtk_container_add (GTK_CONTAINER (box), app);
+
+  app = g_object_new (PHOSH_TYPE_APP,
+                      "app-id", "org.gnome.Nautilus",
+                      "title", "Home",
+                      "win-width", 640,
+                      "win-height", 360,
+                      "max-width", 240,
+                      "max-height", 400,
+                      "visible", TRUE,
+                      NULL);
+  gtk_container_add (GTK_CONTAINER (box), app);
+
+  gtk_main ();
+
+  return 0;
+}
