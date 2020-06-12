6708eca684df77e761e83d42baa6f07b48caaa47
Guido Günther <agx@sigxcpu.org>
2020-06-12 18:27:23 +0200

    lockscreen: Add G_{BEGIN,END}_DECLS

--- src/lockscreen.h
+++ src/lockscreen.h
@@ -7,6 +7,8 @@
 
 #include "layersurface.h"
 
+G_BEGIN_DECLS
+
 #define PHOSH_TYPE_LOCKSCREEN (phosh_lockscreen_get_type ())
 
 G_DECLARE_FINAL_TYPE (PhoshLockscreen, phosh_lockscreen, PHOSH, LOCKSCREEN,
@@ -14,3 +16,4 @@ G_DECLARE_FINAL_TYPE (PhoshLockscreen, phosh_lockscreen, PHOSH, LOCKSCREEN,
 
 GtkWidget * phosh_lockscreen_new (gpointer layer_shell, gpointer wl_output);
 
+G_END_DECLS
