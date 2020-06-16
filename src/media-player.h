042c0f464cc47eba36f94d7c22edd55fb9eae4aa
Guido Günther <agx@sigxcpu.org>
2020-06-16 17:10:24 +0200

    Add media-player widget
    This interfaces with mpris Mediaplayer2 based players
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

---
+++ src/media-player.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2020 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0+
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define PHOSH_TYPE_MEDIA_PLAYER (phosh_media_player_get_type ())
+
+G_DECLARE_FINAL_TYPE (PhoshMediaPlayer, phosh_media_player, PHOSH, MEDIA_PLAYER, GtkGrid)
+
+GtkWidget * phosh_media_player_new (void);
+
+G_END_DECLS
