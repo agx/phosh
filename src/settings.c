3d2ad6963e5ffffd067148c3d15fca17bceeb026
Guido Günther <agx@sigxcpu.org>
2020-06-29 17:35:42 +0200

    Add wwan quicksetting
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

--- src/settings.c
+++ src/settings.c
@@ -109,6 +109,13 @@ wifi_setting_clicked_cb (PhoshSettings *self)
 }
 
 static void
+wwan_setting_clicked_cb (PhoshSettings *self)
+{
+  phosh_quick_setting_open_settings_panel ("wwan");
+  g_signal_emit (self, signals[SETTING_DONE], 0);
+}
+
+static void
 feedback_setting_long_pressed_cb (PhoshSettings *self)
 {
   phosh_quick_setting_open_settings_panel ("notifications");
@@ -391,6 +398,7 @@ phosh_settings_class_init (PhoshSettingsClass *klass)
   gtk_widget_class_bind_template_callback (widget_class, feedback_setting_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, feedback_setting_long_pressed_cb);
   gtk_widget_class_bind_template_callback (widget_class, wifi_setting_clicked_cb);
+  gtk_widget_class_bind_template_callback (widget_class, wwan_setting_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_media_player_raised);
 }
 
