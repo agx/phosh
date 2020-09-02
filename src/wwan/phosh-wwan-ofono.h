3936d76e7cb82d2a9ef76c1faacb2fa168551b85
anteater <nt8r@protonmail.com>
2020-09-02 08:23:18 -0400

    wwan: add PhoshWWanOfono

---
+++ src/wwan/phosh-wwan-ofono.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2020 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+#pragma once
+
+#include <glib-object.h>
+#include "phosh-wwan-iface.h"
+
+G_BEGIN_DECLS
+
+#define PHOSH_TYPE_WWAN_OFONO (phosh_wwan_ofono_get_type())
+
+G_DECLARE_FINAL_TYPE (PhoshWWanOfono, phosh_wwan_ofono, PHOSH, WWAN_OFONO, GObject)
+
+PhoshWWanOfono *phosh_wwan_ofono_new (void);
+
+G_END_DECLS
