5dceba8b4ce6aa33fdbacf210543957be754d531
Guido Günther <agx@sigxcpu.org>
2020-11-24 18:20:51 +0100

    Add PhoshDBusNotification
    This allows us to have notification just do the right thing.
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

---
+++ src/notifications/dbus-notification.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2020 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <notifications/notification.h>
+
+G_BEGIN_DECLS
+
+#define PHOSH_TYPE_DBUS_NOTIFICATION     phosh_dbus_notification_get_type ()
+
+G_DECLARE_FINAL_TYPE (PhoshDBusNotification, phosh_dbus_notification,
+                      PHOSH, DBUS_NOTIFICATION, PhoshNotification)
+
+PhoshDBusNotification    *phosh_dbus_notification_new      (guint                     id,
+                                                            const char               *app_name,
+                                                            GAppInfo                 *info,
+                                                            const char               *summary,
+                                                            const char               *body,
+                                                            GIcon                    *icon,
+                                                            GIcon                    *image,
+                                                            PhoshNotificationUrgency  urgency,
+                                                            GStrv                     actions,
+                                                            gboolean                  transient,
+                                                            gboolean                  resident,
+                                                            const char               *category,
+                                                            GDateTime                *timestamp);
+
+G_END_DECLS
