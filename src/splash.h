1e35884cefeb96fb2c6edf36ce23ed02d9dbc241
Guido Günther <agx@sigxcpu.org>
2021-01-24 17:07:31 +0100

    Add PhoshSplash
    Splash screen for launching applications. It times out after
    5 seconds if not closed.
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

---
+++ src/splash.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+#include "layersurface.h"
+
+#include <gio/gdesktopappinfo.h>
+
+#define PHOSH_TYPE_SPLASH (phosh_splash_get_type ())
+
+G_DECLARE_DERIVABLE_TYPE (PhoshSplash, phosh_splash, PHOSH, SPLASH, PhoshLayerSurface)
+
+/**
+ * PhoshSplashClass
+ * @parent_class: The parent class
+ */
+struct _PhoshSplashClass {
+  PhoshLayerSurfaceClass parent_class;
+};
+
+
+GtkWidget *phosh_splash_new (GDesktopAppInfo *info);
