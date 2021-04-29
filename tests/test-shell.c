33ef1e3a94e124ec21fe34e9c6f59cab64305b9f
Guido Günther <agx@sigxcpu.org>
2021-04-29 12:20:24 +0200

    tests: Create shell object
    This creats the shell object making sure we create most of
    the objects that aren't created in the idle callback()
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

---
+++ tests/test-shell.c
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#include "testlib.h"
+
+#include "log.h"
+#include "shell.h"
+#include "phosh-wayland.h"
+
+
+#include <glib.h>
+#include <handy.h>
+
+
+typedef struct _Fixture {
+  PhoshTestCompositorState *state;
+} Fixture;
+
+
+static void
+compositor_setup (Fixture *fixture, gconstpointer unused)
+{
+  fixture->state = phosh_test_compositor_new ();
+  g_assert_nonnull (fixture->state);
+}
+
+static void
+compositor_teardown (Fixture *fixture, gconstpointer unused)
+{
+  phosh_test_compositor_free (fixture->state);
+}
+
+
+static void
+test_shell_new (Fixture *fixture, gconstpointer unused)
+{
+  PhoshShell *shell = NULL;
+  GLogLevelFlags flags;
+
+  gtk_init (NULL, NULL);
+  hdy_init ();
+
+  phosh_log_set_log_domains ("all");
+
+  /* Drop warnings from the fatal log mask since there's plenty
+   * when running without recommended DBus services */
+  flags = g_log_set_always_fatal (0);
+  g_log_set_always_fatal(flags & ~G_LOG_LEVEL_WARNING);
+
+  shell = phosh_shell_get_default ();
+  g_assert_true (PHOSH_IS_SHELL (shell));
+
+  g_log_set_always_fatal(flags);
+
+  g_assert_false (phosh_shell_get_locked (shell));
+  g_assert_false (phosh_shell_is_startup_finished (shell));
+  g_assert_true (PHOSH_IS_MONITOR (phosh_shell_get_primary_monitor (shell)));
+
+  g_object_unref (shell);
+}
+
+
+int
+main (int   argc,
+      char *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add ("/phosh/shell/new", Fixture, NULL,
+              compositor_setup, test_shell_new, compositor_teardown);
+
+  return g_test_run ();
+}
