c031ca9a8491e952a72453874d99a2ec276dafc6
Guido Günther <agx@sigxcpu.org>
2021-05-11 14:51:15 +0200

    tests: testlib: Unregister timeout handler
    Otherwise it might fire at a later invocation.
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

--- tests/testlib.c
+++ tests/testlib.c
@@ -139,6 +139,7 @@ phosh_test_compositor_new (void)
   gboolean ret;
   int outfd;
   PhocOutputWatch watch;
+  gint id;
 
   comp = g_getenv ("PHOSH_PHOC_BINARY");
   if (!comp) {
@@ -181,8 +182,9 @@ phosh_test_compositor_new (void)
                   &watch);
   g_child_watch_add (state->pid, on_phoc_exit, NULL);
 
-  g_timeout_add_seconds (STARTUP_TIMEOUT, on_phoc_startup_timeout, NULL);
+  id = g_timeout_add_seconds (STARTUP_TIMEOUT, on_phoc_startup_timeout, NULL);
   g_main_loop_run (mainloop);
+  g_source_remove (id);
 
   /* I/O watch in main should have gotten the socket name */
   g_assert (watch.socket);
