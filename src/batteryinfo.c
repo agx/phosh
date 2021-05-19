dc060c6c79fce51a033bdf9e23614972249dcf63
Guido Günther <agx@sigxcpu.org>
2021-05-19 17:40:44 +0200

    batteryinfo: Add optional label with battery percentage
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

--- src/batteryinfo.c
+++ src/batteryinfo.c
@@ -22,14 +22,61 @@
  * @Title: PhoshBatteryInfo
  */
 
+enum {
+  PROP_0,
+  PROP_SHOW_DETAIL,
+  PROP_LAST_PROP
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+
 typedef struct _PhoshBatteryInfo {
-  PhoshStatusIcon parent;
-  UpClient     *upower;
-  UpDevice     *device;
+  PhoshStatusIcon  parent;
+  UpClient        *upower;
+  UpDevice        *device;
+  gboolean         show_detail;
 } PhoshBatteryInfo;
 
 
 G_DEFINE_TYPE (PhoshBatteryInfo, phosh_battery_info, PHOSH_TYPE_STATUS_ICON)
+
+
+static void
+phosh_battery_info_set_property (GObject *object,
+                              guint property_id,
+                              const GValue *value,
+                              GParamSpec *pspec)
+{
+  PhoshBatteryInfo *self = PHOSH_BATTERY_INFO (object);
+
+  switch (property_id) {
+  case PROP_SHOW_DETAIL:
+    phosh_battery_info_set_show_detail (self, g_value_get_boolean (value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+phosh_battery_info_get_property (GObject    *object,
+                                 guint       property_id,
+                                 GValue     *value,
+                                 GParamSpec *pspec)
+{
+  PhoshBatteryInfo *self = PHOSH_BATTERY_INFO (object);
+
+  switch (property_id) {
+  case PROP_SHOW_DETAIL:
+    g_value_set_boolean (value, self->show_detail);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
 
 
 static void
@@ -82,6 +129,11 @@ phosh_battery_info_constructed (GObject *object)
                             NULL,
                             NULL,
                             NULL);
+    g_object_bind_property (self,
+                            "info",
+                            phosh_status_icon_get_extra_widget (PHOSH_STATUS_ICON (self)),
+                            "label",
+                            G_BINDING_SYNC_CREATE);
   }
 }
 
@@ -106,14 +158,34 @@ phosh_battery_info_class_init (PhoshBatteryInfoClass *klass)
 
   object_class->constructed = phosh_battery_info_constructed;
   object_class->dispose = phosh_battery_info_dispose;
+  object_class->get_property = phosh_battery_info_get_property;
+  object_class->set_property = phosh_battery_info_set_property;
 
   gtk_widget_class_set_css_name (widget_class, "phosh-battery-info");
+
+  props[PROP_SHOW_DETAIL] =
+    g_param_spec_boolean (
+      "show-detail",
+      "",
+      "",
+      FALSE,
+      G_PARAM_CONSTRUCT | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 }
 
 
 static void
 phosh_battery_info_init (PhoshBatteryInfo *self)
 {
+  GtkWidget *percentage = gtk_label_new (NULL);
+  phosh_status_icon_set_extra_widget (PHOSH_STATUS_ICON (self), percentage);
+
+  g_object_bind_property (self,
+                          "show-detail",
+                          percentage,
+                          "visible",
+                          G_BINDING_SYNC_CREATE);
 }
 
 
@@ -121,4 +193,26 @@ GtkWidget *
 phosh_battery_info_new (void)
 {
   return g_object_new (PHOSH_TYPE_BATTERY_INFO, NULL);
+}
+
+
+void
+phosh_battery_info_set_show_detail (PhoshBatteryInfo *self, gboolean show)
+{
+  g_return_if_fail (PHOSH_IS_BATTERY_INFO (self));
+
+  if (self->show_detail == show)
+    return;
+
+  self->show_detail = !!show;
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_SHOW_DETAIL]);
+}
+
+
+gboolean
+phosh_battery_info_get_show_detail (PhoshBatteryInfo *self)
+{
+  g_return_val_if_fail (PHOSH_IS_BATTERY_INFO (self), FALSE);
+
+  return self->show_detail;
 }
