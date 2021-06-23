970d9d8f4bc86e9a2a63bbbc9333e088b3a5e1b5
Guido Günther <agx@sigxcpu.org>
2021-06-23 08:43:50 +0200

    overview: Add missing G_{BEGIN,END}_DECLS
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

--- src/overview.h
+++ src/overview.h
@@ -8,6 +8,8 @@
 
 #include <gtk/gtk.h>
 
+G_BEGIN_DECLS
+
 #define PHOSH_TYPE_OVERVIEW (phosh_overview_get_type())
 
 G_DECLARE_FINAL_TYPE (PhoshOverview, phosh_overview, PHOSH, OVERVIEW, GtkBox)
@@ -17,3 +19,5 @@ GtkWidget *phosh_overview_new (void);
 void       phosh_overview_reset (PhoshOverview *self);
 void       phosh_overview_focus_app_search (PhoshOverview *self);
 gboolean   phosh_overview_has_running_activities (PhoshOverview *self);
+
+G_END_DECLS
