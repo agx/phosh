agx/phosh

commit 009177fac12b41d780ffc255bbb1105915407ead

overview: Focus current activity
This allows to keyboard navigate on the home screen

Signed-off-by: Guido Günther <guido.gunther@puri.sm>

src/overview.c
@@ -52,6 +52,7 @@ typedef struct
   /* Running activities */
   GtkWidget *carousel_running_activities;
   GtkWidget *app_grid;
+  GtkWidget *activity;
 } PhoshOverviewPrivate;
 
 
@@ -157,8 +158,10 @@ on_toplevel_activated_changed (PhoshToplevel *toplevel, GParamSpec *pspec, Phosh
   priv = phosh_overview_get_instance_private (overview);
 
   activity = find_activity_by_toplevel (overview, toplevel);
-  if (phosh_toplevel_is_activated (toplevel))
+  if (phosh_toplevel_is_activated (toplevel)) {
+    priv->activity = GTK_WIDGET (activity);
     hdy_carousel_scroll_to (HDY_CAROUSEL (priv->carousel_running_activities), GTK_WIDGET (activity));
+  }
 }
 
 
@@ -191,6 +194,20 @@ static void
 on_activity_size_allocated (PhoshActivity *activity, GtkAllocation *alloc, PhoshToplevel *toplevel)
 {
   request_thumbnail (activity, toplevel);
+}
+
+
+static void
+on_activity_has_focus_changed (PhoshOverview *self, GParamSpec *pspec, PhoshActivity *activity)
+{
+  PhoshOverviewPrivate *priv;
+
+  g_return_if_fail (PHOSH_IS_ACTIVITY (activity));
+  g_return_if_fail (PHOSH_IS_OVERVIEW (self));
+  priv = phosh_overview_get_instance_private (self);
+
+  if (gtk_widget_has_focus (GTK_WIDGET (activity)))
+    hdy_carousel_scroll_to (HDY_CAROUSEL (priv->carousel_running_activities), GTK_WIDGET (activity));
 }
 
 
@@ -229,11 +246,14 @@ add_activity (PhoshOverview *self, PhoshToplevel *toplevel)
   g_object_bind_property (toplevel, "maximized", activity, "maximized", G_BINDING_DEFAULT);
 
   g_signal_connect (activity, "size-allocate", G_CALLBACK (on_activity_size_allocated), toplevel);
+  g_signal_connect_swapped (activity, "notify::has-focus", G_CALLBACK (on_activity_has_focus_changed), self);
 
   phosh_connect_button_feedback (GTK_BUTTON (activity));
 
-  if (phosh_toplevel_is_activated (toplevel))
+  if (phosh_toplevel_is_activated (toplevel)) {
     hdy_carousel_scroll_to (HDY_CAROUSEL (priv->carousel_running_activities), activity);
+    priv->activity = GTK_WIDGET (activity);
+  }
 }
 
 
@@ -422,6 +442,9 @@ phosh_overview_reset (PhoshOverview *self)
   g_return_if_fail(PHOSH_IS_OVERVIEW (self));
   priv = phosh_overview_get_instance_private (self);
   phosh_app_grid_reset (PHOSH_APP_GRID (priv->app_grid));
+
+  if (priv->activity)
+    gtk_widget_grab_focus (GTK_WIDGET (priv->activity));
 }
 
 

commit 9c2e3b09a13360f5469c78b6db0ad50ae8f2cf29

overview: Unset activity when toplevel goes away
This prevents us from trying to set a focus on an
inexistent activity in phosh_overview_reset.

Fixes: 009177fac12b41d780ffc255bbb1105915407ead

Signed-off-by: Guido Günther <guido.gunther@puri.sm>

src/overview.c
@@ -170,11 +170,21 @@ on_activity_closed (PhoshOverview *self, PhoshActivity *activity)
 
 
 static void
-on_toplevel_closed (PhoshToplevel *toplevel, PhoshActivity *activity)
+on_toplevel_closed (PhoshToplevel *toplevel, PhoshOverview *overview)
 {
+  PhoshActivity *activity;
+  PhoshOverviewPrivate *priv;
+
   g_return_if_fail (PHOSH_IS_TOPLEVEL (toplevel));
+  g_return_if_fail (PHOSH_IS_OVERVIEW (overview));
+  priv = phosh_overview_get_instance_private (overview);
+
+  activity = find_activity_by_toplevel (overview, toplevel);
   g_return_if_fail (PHOSH_IS_ACTIVITY (activity));
   gtk_widget_destroy (GTK_WIDGET (activity));
+
+  if (priv->activity == activity)
+    priv->activity = NULL;
 }
 
 
@@ -271,7 +281,7 @@ add_activity (PhoshOverview *self, PhoshToplevel *toplevel)
   g_signal_connect_swapped (activity, "closed",
                             G_CALLBACK (on_activity_closed), self);
 
-  g_signal_connect_object (toplevel, "closed", G_CALLBACK (on_toplevel_closed), activity, 0);
+  g_signal_connect_object (toplevel, "closed", G_CALLBACK (on_toplevel_closed), self, 0);
   g_signal_connect_object (toplevel, "notify::activated", G_CALLBACK (on_toplevel_activated_changed), self, 0);
   g_object_bind_property (toplevel, "maximized", activity, "maximized", G_BINDING_DEFAULT);
 