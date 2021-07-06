agx/phosh

commit a749990f0c37c7e361f6c1df945053b0432d0415

monitor: Move DisplayConfig DBus sources to src/dbus
This gets them ignored when calcualting coverage but also gets us closer
to have all DBus generation in once directory and meson variable.

We keep file and interface names unchanged for minimal diff noise.

Signed-off-by: Guido Günther <guido.gunther@puri.sm>

src/dbus/meson.build
@@ -84,6 +84,11 @@ generated_dbus_sources += gnome.gdbus_codegen('phosh-gnome-shell-dbus',
 					      interface_prefix: 'org.gnome',
 					      namespace: 'PhoshGnomeShellDBus')
 
+generated_dbus_sources += gnome.gdbus_codegen('phosh-display-dbus',
+                                              'org.gnome.Mutter.DisplayConfig.xml',
+                                              interface_prefix: 'org.gnome.Mutter',
+                                              namespace: 'PhoshDisplayDbus')
+
 generated_dbus_sources += gnome.gdbus_codegen('phosh-idle-dbus',
 					      'org.gnome.Mutter.IdleMonitor.xml',
 					      interface_prefix: 'org.gnome.Mutter',

src/monitor-manager.h
@@ -7,7 +7,7 @@
  */
 #pragma once
 
-#include "monitor/phosh-display-dbus.h"
+#include "phosh-display-dbus.h"
 #include "monitor/monitor.h"
 
 #include "sensor-proxy-manager.h"

src/monitor/meson.build
@@ -1,14 +1,6 @@
-generated_monitor_sources = gnome.gdbus_codegen(
-  'phosh-display-dbus',
-  'org.gnome.Mutter.DisplayConfig.xml',
-  interface_prefix: 'org.gnome.Mutter',
-  namespace: 'PhoshDisplayDbus',
-)
-
 phosh_monitor_sources = [
   'monitor/head.c',
   'monitor/head.h',
   'monitor/monitor.c',
   'monitor/monitor.h',
-  generated_monitor_sources,
 ]

commit 488393b05d8ef95b1d953ce0d084da3478204ac6

monitor-manager: Adjust DBus naming
Use DBus instead of Dbus as with all other protocols and drop
use PhoshDBus as namespace which we want to use for all protocols
that don't need an object manager.

Reindent the interface function arguments while at that.

Fixes: a749990f0c37c7e361f6c1df945053b0432d0415
Signed-off-by: Guido Günther <guido.gunther@puri.sm>

src/dbus/meson.build
@@ -97,7 +97,7 @@ generated_dbus_sources += gnome.gdbus_codegen('phosh-gnome-shell-dbus',
 generated_dbus_sources += gnome.gdbus_codegen('phosh-display-dbus',
                                               'org.gnome.Mutter.DisplayConfig.xml',
                                               interface_prefix: 'org.gnome.Mutter',
-                                              namespace: 'PhoshDisplayDbus')
+                                              namespace: 'PhoshDBus')
 
 generated_dbus_sources += gnome.gdbus_codegen('phosh-idle-dbus',
 					      'org.gnome.Mutter.IdleMonitor.xml',

src/monitor-manager.c
@@ -27,7 +27,7 @@
  *
  * This keeps track of all monitors and handles the
  * org.gnome.Mutter.DisplayConfig DBus interface via
- * #PhoshDisplayDbusDisplayConfig. This includes individual monitor
+ * #PhoshDBusDisplayConfig. This includes individual monitor
  * configuration as well as blanking/power saving.
  */
 
@@ -53,11 +53,11 @@ enum {
 static guint signals[N_SIGNALS] = { 0 };
 
 static void phosh_monitor_manager_display_config_init (
-  PhoshDisplayDbusDisplayConfigIface *iface);
+  PhoshDBusDisplayConfigIface *iface);
 
 typedef struct _PhoshMonitorManager
 {
-  PhoshDisplayDbusDisplayConfigSkeleton parent;
+  PhoshDBusDisplayConfigSkeleton parent;
 
   PhoshSensorProxyManager *sensor_proxy_manager;
   GBinding                *sensor_proxy_binding;
@@ -73,9 +73,9 @@ typedef struct _PhoshMonitorManager
 
 G_DEFINE_TYPE_WITH_CODE (PhoshMonitorManager,
                          phosh_monitor_manager,
-                         PHOSH_DISPLAY_DBUS_TYPE_DISPLAY_CONFIG_SKELETON,
+                         PHOSH_DBUS_TYPE_DISPLAY_CONFIG_SKELETON,
                          G_IMPLEMENT_INTERFACE (
-                           PHOSH_DISPLAY_DBUS_TYPE_DISPLAY_CONFIG,
+                           PHOSH_DBUS_TYPE_DISPLAY_CONFIG,
                            phosh_monitor_manager_display_config_init));
 
 
@@ -195,9 +195,8 @@ phosh_monitor_manager_flip_transform (PhoshMonitorTransform transform)
  */
 
 static gboolean
-phosh_monitor_manager_handle_get_resources (
-  PhoshDisplayDbusDisplayConfig *skeleton,
-  GDBusMethodInvocation *invocation)
+phosh_monitor_manager_handle_get_resources (PhoshDBusDisplayConfig *skeleton,
+                                            GDBusMethodInvocation  *invocation)
 {
   PhoshMonitorManager *self = PHOSH_MONITOR_MANAGER (skeleton);
   GVariantBuilder crtc_builder, output_builder, mode_builder;
@@ -291,7 +290,7 @@ phosh_monitor_manager_handle_get_resources (
 
   /* Don't bother setting up modes, they're ignored */
 
-  phosh_display_dbus_display_config_complete_get_resources (
+  phosh_dbus_display_config_complete_get_resources (
     skeleton,
     invocation,
     self->serial,
@@ -307,12 +306,11 @@ phosh_monitor_manager_handle_get_resources (
 
 
 static gboolean
-phosh_monitor_manager_handle_change_backlight (
-  PhoshDisplayDbusDisplayConfig *skeleton,
-  GDBusMethodInvocation *invocation,
-  guint                  serial,
-  guint                  output_index,
-  int                    value)
+phosh_monitor_manager_handle_change_backlight (PhoshDBusDisplayConfig *skeleton,
+                                               GDBusMethodInvocation  *invocation,
+                                               guint                   serial,
+                                               guint                   output_index,
+                                               int                     value)
 {
   g_debug ("Unimplemented DBus call %s", __func__);
   g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR,
@@ -323,7 +321,7 @@ phosh_monitor_manager_handle_change_backlight (
 
 
 struct get_wl_gamma_callback_data {
-  PhoshDisplayDbusDisplayConfig *skeleton;
+  PhoshDBusDisplayConfig *skeleton;
   GDBusMethodInvocation *invocation;
 };
 
@@ -348,7 +346,7 @@ static void handle_wl_gamma_size(void *data, struct gamma_control *gamma_control
   green_v = g_variant_new_from_bytes (G_VARIANT_TYPE ("aq"), green_bytes, TRUE);
   blue_v = g_variant_new_from_bytes (G_VARIANT_TYPE ("aq"), blue_bytes, TRUE);
 
-  phosh_display_dbus_display_config_complete_get_crtc_gamma (
+  phosh_dbus_display_config_complete_get_crtc_gamma (
     gamma_callback_data->skeleton,
     gamma_callback_data->invocation,
     red_v, green_v, blue_v);
@@ -369,11 +367,10 @@ gamma_control_listener gamma_control_listener = {
 
 
 static gboolean
-phosh_monitor_manager_handle_get_crtc_gamma (
-  PhoshDisplayDbusDisplayConfig *skeleton,
-  GDBusMethodInvocation *invocation,
-  guint                  serial,
-  guint                  crtc_id)
+phosh_monitor_manager_handle_get_crtc_gamma (PhoshDBusDisplayConfig *skeleton,
+                                             GDBusMethodInvocation  *invocation,
+                                             guint                   serial,
+                                             guint                   crtc_id)
 {
   PhoshMonitorManager *self = PHOSH_MONITOR_MANAGER (skeleton);
   PhoshMonitor *monitor;
@@ -421,14 +418,13 @@ phosh_monitor_manager_handle_get_crtc_gamma (
 
 
 static gboolean
-phosh_monitor_manager_handle_set_crtc_gamma (
-  PhoshDisplayDbusDisplayConfig *skeleton,
-  GDBusMethodInvocation *invocation,
-  guint                  serial,
-  guint                  crtc_id,
-  GVariant              *red_v,
-  GVariant              *green_v,
-  GVariant              *blue_v)
+phosh_monitor_manager_handle_set_crtc_gamma (PhoshDBusDisplayConfig *skeleton,
+                                             GDBusMethodInvocation  *invocation,
+                                             guint                   serial,
+                                             guint                   crtc_id,
+                                             GVariant               *red_v,
+                                             GVariant               *green_v,
+                                             GVariant               *blue_v)
 {
   PhoshMonitorManager *self = PHOSH_MONITOR_MANAGER (skeleton);
   PhoshMonitor *monitor;
@@ -499,7 +495,7 @@ phosh_monitor_manager_handle_set_crtc_gamma (
   gamma_control_set_gamma(gamma_control, &wl_red, &wl_green, &wl_blue);
   gamma_control_destroy (gamma_control);
 
-  phosh_display_dbus_display_config_complete_set_crtc_gamma (
+  phosh_dbus_display_config_complete_set_crtc_gamma (
       skeleton,
       invocation);
 
@@ -527,9 +523,8 @@ phosh_monitor_manager_handle_set_crtc_gamma (
 
 
 static gboolean
-phosh_monitor_manager_handle_get_current_state (
-  PhoshDisplayDbusDisplayConfig *skeleton,
-  GDBusMethodInvocation *invocation)
+phosh_monitor_manager_handle_get_current_state (PhoshDBusDisplayConfig *skeleton,
+                                                GDBusMethodInvocation  *invocation)
 {
   PhoshMonitorManager *self = PHOSH_MONITOR_MANAGER (skeleton);
   GVariantBuilder monitors_builder, logical_monitors_builder, properties_builder;
@@ -672,7 +667,7 @@ phosh_monitor_manager_handle_get_current_state (
                          "supports-changing-layout-mode",
                          g_variant_new_boolean (TRUE));
 
-  phosh_display_dbus_display_config_complete_get_current_state (
+  phosh_dbus_display_config_complete_get_current_state (
     skeleton,
     invocation,
     self->serial,
@@ -821,13 +816,12 @@ config_head_config_from_logical_monitor_variant (PhoshMonitorManager *self,
 
 
 static gboolean
-phosh_monitor_manager_handle_apply_monitors_config (
-  PhoshDisplayDbusDisplayConfig *skeleton,
-  GDBusMethodInvocation *invocation,
-  guint                  serial,
-  guint                  method,
-  GVariant              *logical_monitor_configs_variant,
-  GVariant              *properties_variant)
+phosh_monitor_manager_handle_apply_monitors_config (PhoshDBusDisplayConfig *skeleton,
+                                                    GDBusMethodInvocation  *invocation,
+                                                    guint                   serial,
+                                                    guint                   method,
+                                                    GVariant               *logical_monitor_configs_variant,
+                                                    GVariant               *properties_variant)
 {
   PhoshMonitorManager *self = PHOSH_MONITOR_MANAGER (skeleton);
   GVariantIter logical_monitor_configs_iter;
@@ -943,7 +937,7 @@ phosh_monitor_manager_handle_apply_monitors_config (
     phosh_monitor_manager_apply_monitor_config (self);
   }
 
-  phosh_display_dbus_display_config_complete_apply_monitors_config (
+  phosh_dbus_display_config_complete_apply_monitors_config (
     skeleton,
     invocation);
 
@@ -952,7 +946,7 @@ phosh_monitor_manager_handle_apply_monitors_config (
 
 
 static void
-phosh_monitor_manager_display_config_init (PhoshDisplayDbusDisplayConfigIface *iface)
+phosh_monitor_manager_display_config_init (PhoshDBusDisplayConfigIface *iface)
 {
   iface->handle_get_resources = phosh_monitor_manager_handle_get_resources;
   iface->handle_change_backlight = phosh_monitor_manager_handle_change_backlight;
@@ -970,8 +964,8 @@ power_save_mode_changed_cb (PhoshMonitorManager *self,
 {
   int mode, ps_mode;
 
-  mode = phosh_display_dbus_display_config_get_power_save_mode (
-    PHOSH_DISPLAY_DBUS_DISPLAY_CONFIG (self));
+  mode = phosh_dbus_display_config_get_power_save_mode (
+    PHOSH_DBUS_DISPLAY_CONFIG (self));
   g_debug ("Power save mode %d requested", mode);
 
   switch (mode) {
@@ -1126,7 +1120,7 @@ on_head_finished (PhoshMonitorManager *self,
   else
     g_warning ("Tried to remove inexistend head %p", head);
 
-  phosh_display_dbus_display_config_emit_monitors_changed (PHOSH_DISPLAY_DBUS_DISPLAY_CONFIG (self));
+  phosh_dbus_display_config_emit_monitors_changed (PHOSH_DBUS_DISPLAY_CONFIG (self));
 }
 
 
@@ -1145,7 +1139,7 @@ zwlr_output_manager_v1_handle_head (void *data,
   g_ptr_array_add (self->heads, head);
   g_signal_connect_swapped (head, "head-finished", G_CALLBACK (on_head_finished), self);
 
-  phosh_display_dbus_display_config_emit_monitors_changed (PHOSH_DISPLAY_DBUS_DISPLAY_CONFIG (self));
+  phosh_dbus_display_config_emit_monitors_changed (PHOSH_DBUS_DISPLAY_CONFIG (self));
 }
 
 
@@ -1161,7 +1155,7 @@ zwlr_output_manager_v1_handle_done (void *data,
   self->zwlr_output_serial = serial;
   self->serial++;
 
-  phosh_display_dbus_display_config_emit_monitors_changed (PHOSH_DISPLAY_DBUS_DISPLAY_CONFIG (self));
+  phosh_dbus_display_config_emit_monitors_changed (PHOSH_DBUS_DISPLAY_CONFIG (self));
 }
 
 

src/monitor-manager.h
@@ -33,7 +33,7 @@ typedef enum _MetaMonitorsConfigMethod
 
 #define PHOSH_TYPE_MONITOR_MANAGER                 (phosh_monitor_manager_get_type ())
 G_DECLARE_FINAL_TYPE (PhoshMonitorManager, phosh_monitor_manager, PHOSH, MONITOR_MANAGER,
-                      PhoshDisplayDbusDisplayConfigSkeleton)
+                      PhoshDBusDisplayConfigSkeleton)
 
 PhoshMonitorManager * phosh_monitor_manager_new                       (PhoshSensorProxyManager *proxy);
 PhoshMonitor        * phosh_monitor_manager_get_monitor               (PhoshMonitorManager *self,

tests/test-monitor-manager.c
@@ -105,7 +105,7 @@ static void
 test_phosh_monitor_manager_current_state (Fixture *fixture, gconstpointer unused)
 {
   g_autoptr (GError) err = NULL;
-  g_autoptr (PhoshDisplayDbusDisplayConfig) proxy = NULL;
+  g_autoptr (PhoshDBusDisplayConfig) proxy = NULL;
   g_autoptr (GVariant) monitors = NULL;
   g_autoptr (GVariant) logical = NULL;
   g_autoptr (GVariant) props = NULL;
@@ -116,22 +116,22 @@ test_phosh_monitor_manager_current_state (Fixture *fixture, gconstpointer unused
   /* Wait until comp/shell are up */
   g_assert_nonnull (g_async_queue_timeout_pop (fixture->queue, POP_TIMEOUT));
 
-  proxy = phosh_display_dbus_display_config_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
-                                                                    G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE,
-                                                                    BUS_NAME,
-                                                                    OBJECT_PATH,
-                                                                    NULL,
-                                                                    &err);
+  proxy = phosh_dbus_display_config_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
+                                                            G_DBUS_OBJECT_MANAGER_CLIENT_FLAGS_NONE,
+                                                            BUS_NAME,
+                                                            OBJECT_PATH,
+                                                            NULL,
+                                                            &err);
   g_assert_no_error (err);
-  g_assert_true (PHOSH_DISPLAY_DBUS_IS_DISPLAY_CONFIG_PROXY (proxy));
+  g_assert_true (PHOSH_DBUS_IS_DISPLAY_CONFIG_PROXY (proxy));
 
-  success = phosh_display_dbus_display_config_call_get_current_state_sync (proxy,
-                                                                           &serial,
-                                                                           &monitors,
-                                                                           &logical,
-                                                                           &props,
-                                                                           NULL,
-                                                                           &err);
+  success = phosh_dbus_display_config_call_get_current_state_sync (proxy,
+                                                                   &serial,
+                                                                   &monitors,
+                                                                   &logical,
+                                                                   &props,
+                                                                   NULL,
+                                                                   &err);
   g_assert_no_error (err);
   g_assert_true (success);
   g_assert_cmpint (serial, ==, 2);