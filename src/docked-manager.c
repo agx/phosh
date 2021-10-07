3b688d164fd1b0d78f8801aeebcaf614c168ce9a
Guido Günther <agx@sigxcpu.org>
2021-10-07 19:24:48 +0200

    docked-manager: Treat unknown hardware as undocked
    If modem-manager doesn't have enough information to set a hardware
    type (no logind, etc) the assume undocked rather than docked since
    we want to default to phone mode.
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>

--- src/docked-manager.c
+++ src/docked-manager.c
@@ -119,10 +119,11 @@ mode_changed_cb (PhoshDockedManager *self, GParamSpec *pspec, PhoshModeManager *
 
   /*
    * Desktops, laptops and phones with enough external hardware should get floating
-   * windows, etc
+   * windows, etc. Unknown hardware is assumed to be a phone.
    */
   if (phosh_mode_manager_get_mimicry (manager) != PHOSH_MODE_DEVICE_TYPE_PHONE &&
-      phosh_mode_manager_get_mimicry (manager) != PHOSH_MODE_DEVICE_TYPE_TABLET)
+      phosh_mode_manager_get_mimicry (manager) != PHOSH_MODE_DEVICE_TYPE_TABLET &&
+      phosh_mode_manager_get_mimicry (manager) != PHOSH_MODE_DEVICE_TYPE_UNKNOWN)
     can_dock = TRUE;
 
   if (can_dock == self->can_dock)
