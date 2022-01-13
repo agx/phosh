32a3ca056c50f0ef070110c202f2b79b094e7103
PanzerSajt <panzersajt.windowslive.com>
2022-01-13 20:08:52 +0100

    lockscreen: Reset inactivity timeout after revealing the OSK
    To prevent sudden scrollbacks to the Info screen, if the OSK was revealed right before the timeout.
    
    Closes: https://gitlab.gnome.org/World/Phosh/phosh/-/issues/689
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/984>

--- src/lockscreen.c
+++ src/lockscreen.c
@@ -299,6 +299,18 @@ delete_button_clicked_cb (PhoshLockscreen *self,
   clear_input (self, FALSE);
 }
 
+static void
+osk_button_clicked_cb (PhoshLockscreen *self,
+                       GtkWidget       *widget)
+{
+  PhoshLockscreenPrivate *priv;
+
+  g_assert (PHOSH_IS_LOCKSCREEN (self));
+  priv = phosh_lockscreen_get_instance_private (self);
+
+  priv->last_input = g_get_monotonic_time ();
+}
+
 
 static void
 long_press_del_cb (PhoshLockscreen *self,
@@ -761,6 +773,7 @@ phosh_lockscreen_class_init (PhoshLockscreenClass *klass)
 
   gtk_widget_class_bind_template_callback (widget_class, long_press_del_cb);
   gtk_widget_class_bind_template_callback (widget_class, delete_button_clicked_cb);
+  gtk_widget_class_bind_template_callback (widget_class, osk_button_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, submit_cb);
   gtk_widget_class_bind_template_callback (widget_class, input_changed_cb);
 
