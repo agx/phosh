agx/phosh

commit 70dd1f2825d18a23337fb56dad9c469f2858bc29

phosh-call: Add DBus based implementation of the call-ui-call interface
This wraps the DBus interface in a PhoshCall's object that implements
CuiCall so it can be used with CuiCallDisplay.

Signed-off-by: Guido Günther <guido.gunther@puri.sm>

src/call.c
@@ -1,0 +1,310 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#define G_LOG_DOMAIN "phosh-call"
+
+#include "call.h"
+#include "util.h"
+
+#include <cui-call.h>
+
+
+enum {
+  PROP_0,
+  PROP_DBUS_PROXY,
+  PROP_DISPLAY_NAME,
+  PROP_ID,
+  PROP_STATE,
+  PROP_ENCRYPTED,
+  PROP_LAST_PROP = PROP_DISPLAY_NAME,
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+
+typedef struct _PhoshCall {
+  GObject                  parent;
+
+  PhoshCallsDBusCallsCall *proxy; /* DBus proxy to a single call on for gnome-calls' DBus service */
+  GCancellable            *cancel;
+} PhoshCall;
+
+
+static void phosh_call_cui_call_interface_init (CuiCallInterface *iface);
+G_DEFINE_TYPE_WITH_CODE (PhoshCall, phosh_call, G_TYPE_OBJECT,
+                         G_IMPLEMENT_INTERFACE (CUI_TYPE_CALL,
+                                                phosh_call_cui_call_interface_init))
+
+
+static const char *
+phosh_call_get_id (CuiCall *call)
+{
+  PhoshCall *self;
+
+  g_return_val_if_fail (PHOSH_IS_CALL (call), NULL);
+  self = PHOSH_CALL (call);
+
+  return phosh_calls_dbus_calls_call_get_id (self->proxy);
+}
+
+
+static const char *
+phosh_call_get_display_name (CuiCall *call)
+{
+  PhoshCall *self;
+
+  g_return_val_if_fail (PHOSH_IS_CALL (call), NULL);
+  self = PHOSH_CALL (call);
+
+  return phosh_calls_dbus_calls_call_get_display_name (self->proxy);
+}
+
+
+static CuiCallState
+phosh_call_get_state (CuiCall *call)
+{
+  PhoshCall *self;
+
+  g_return_val_if_fail (PHOSH_IS_CALL (call), CUI_CALL_STATE_UNKNOWN);
+  self = PHOSH_CALL (call);
+
+  return phosh_calls_dbus_calls_call_get_state (self->proxy);
+}
+
+
+static gboolean
+phosh_call_get_encrypted (CuiCall *call)
+{
+  PhoshCall *self;
+
+  g_return_val_if_fail (PHOSH_IS_CALL (call), CUI_CALL_STATE_UNKNOWN);
+  self = PHOSH_CALL (call);
+
+  return phosh_calls_dbus_calls_call_get_encrypted (self->proxy);
+}
+
+static void
+on_prop_changed (PhoshCall *self, GParamSpec *pspec)
+{
+  const char *name = g_param_spec_get_name (pspec);
+
+  /* Just forward any property changes, we fetch them from the DBus proxy anyway */
+  if (g_strcmp0 (name, "state") == 0 ||
+      g_strcmp0 (name, "encrypted") == 0 ||
+      g_strcmp0 (name, "id") == 0 ||
+      g_strcmp0 (name, "display-name") == 0) {
+    g_object_notify (G_OBJECT (self), name);
+  }
+}
+
+
+static void
+phosh_call_set_dbus_proxy (PhoshCall *self, PhoshCallsDBusCallsCall *proxy)
+{
+  self->proxy = g_object_ref (proxy);
+
+  g_object_connect (self->proxy,
+                    "swapped-signal::notify::state", G_CALLBACK (on_prop_changed), self,
+                    "swapped-signal::notify::encrypted", G_CALLBACK (on_prop_changed), self,
+                    "swapped-signal::notify::id", G_CALLBACK (on_prop_changed), self,
+                    "swapped-signal::notify::display-name", G_CALLBACK (on_prop_changed), self,
+                    NULL);
+
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_DBUS_PROXY]);
+}
+
+
+static void
+phosh_call_set_property (GObject      *object,
+                         guint         property_id,
+                         const GValue *value,
+                         GParamSpec   *pspec)
+{
+  PhoshCall *self = PHOSH_CALL (object);
+
+  switch (property_id) {
+  case PROP_DBUS_PROXY:
+    /* construct only */
+    phosh_call_set_dbus_proxy (self, g_value_get_object (value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+phosh_call_get_property (GObject    *object,
+                         guint       property_id,
+                         GValue     *value,
+                         GParamSpec *pspec)
+{
+  PhoshCall *self = PHOSH_CALL (object);
+  CuiCall *iface = CUI_CALL (object);
+
+  switch (property_id) {
+  case PROP_DBUS_PROXY:
+    g_value_set_object (value, self->proxy);
+    break;
+  case PROP_ID:
+    g_value_set_string (value, phosh_call_get_id (iface));
+    break;
+  case PROP_DISPLAY_NAME:
+    g_value_set_string (value, phosh_call_get_display_name (iface));
+    break;
+  case PROP_STATE:
+    g_value_set_enum (value, phosh_call_get_state (iface));
+    break;
+  case PROP_ENCRYPTED:
+    g_value_set_boolean (value, phosh_call_get_encrypted (iface));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+phosh_call_dispose (GObject *object)
+{
+  PhoshCall *self = PHOSH_CALL (object);
+
+  g_cancellable_cancel (self->cancel);
+  g_clear_object (&self->cancel);
+  g_signal_handlers_disconnect_by_data (self->proxy, self);
+  g_clear_object (&self->proxy);
+
+  G_OBJECT_CLASS (phosh_call_parent_class)->dispose (object);
+}
+
+
+static void
+phosh_call_class_init (PhoshCallClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->dispose = phosh_call_dispose;
+  object_class->set_property = phosh_call_set_property;
+  object_class->get_property = phosh_call_get_property;
+
+  /**
+   * PhoshCall:dbus-proxy:
+   *
+   * The DBus proxy object to a call on gnome-calls DBus interface
+   */
+  props[PROP_DBUS_PROXY] = g_param_spec_object ("dbus-proxy",
+                                                "",
+                                                "",
+                                                PHOSH_CALLS_DBUS_TYPE_CALLS_CALL,
+                                                G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS |
+                                                G_PARAM_CONSTRUCT_ONLY);
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
+
+  g_object_class_override_property (object_class,
+                                    PROP_DISPLAY_NAME,
+                                    "id");
+
+  g_object_class_override_property (object_class,
+                                    PROP_ID,
+                                    "display-name");
+
+  g_object_class_override_property (object_class,
+                                    PROP_STATE,
+                                    "state");
+
+  g_object_class_override_property (object_class,
+				    PROP_ENCRYPTED,
+                                    "encrypted");
+}
+
+
+static void
+on_call_accept_finish (PhoshCallsDBusCallsCall *proxy,
+                       GAsyncResult            *res,
+                       gpointer                 unused)
+{
+  g_autoptr (GError) err = NULL;
+
+  g_return_if_fail (PHOSH_CALLS_DBUS_IS_CALLS_CALL_PROXY (proxy));
+
+  if (!phosh_calls_dbus_calls_call_call_accept_finish (proxy, res, &err))
+    phosh_async_error_warn (err, "Failed to accept call %p", proxy);
+}
+
+
+static void
+phosh_call_accept (CuiCall *call)
+{
+  PhoshCall *self;
+
+  g_return_if_fail (PHOSH_IS_CALL (call));
+  self = PHOSH_CALL (call);
+
+  phosh_calls_dbus_calls_call_call_accept (self->proxy,
+                                           self->cancel,
+                                           (GAsyncReadyCallback) on_call_accept_finish,
+                                           NULL);
+}
+
+static void
+on_call_hangup_finish (PhoshCallsDBusCallsCall *proxy,
+                       GAsyncResult            *res,
+                       gpointer                 unused)
+{
+  g_autoptr (GError) err = NULL;
+
+  g_return_if_fail (PHOSH_CALLS_DBUS_IS_CALLS_CALL_PROXY (proxy));
+
+  if (!phosh_calls_dbus_calls_call_call_hangup_finish (proxy, res, &err))
+    phosh_async_error_warn (err, "Failed to hangup call %p", proxy);
+}
+
+
+static void
+phosh_call_hang_up (CuiCall *call)
+{
+  PhoshCall *self;
+
+  g_return_if_fail (PHOSH_IS_CALL (call));
+  self = PHOSH_CALL (call);
+
+  phosh_calls_dbus_calls_call_call_hangup (self->proxy,
+                                           self->cancel,
+                                           (GAsyncReadyCallback) on_call_hangup_finish,
+                                           NULL);
+}
+
+
+static void
+phosh_call_cui_call_interface_init (CuiCallInterface *iface)
+{
+  iface->get_id = phosh_call_get_id;
+  iface->get_display_name = phosh_call_get_display_name;
+  iface->get_state = phosh_call_get_state;
+  iface->get_encrypted = phosh_call_get_encrypted;
+
+  iface->accept = phosh_call_accept;
+  iface->hang_up = phosh_call_hang_up;
+}
+
+
+static void
+phosh_call_init (PhoshCall *self)
+{
+  self->cancel = g_cancellable_new ();
+}
+
+
+PhoshCall *
+phosh_call_new (PhoshCallsDBusCallsCall *proxy)
+{
+  return g_object_new (PHOSH_TYPE_CALL,
+                       "dbus-proxy", proxy,
+                       NULL);
+}

docs/phosh-docs.xml
@@ -50,6 +50,7 @@
       <xi:include href="xml/batteryinfo.xml"/>
       <xi:include href="xml/bt-manager.xml"/>
       <xi:include href="xml/bt-info.xml"/>
+      <xi:include href="xml/call.xml"/>
       <xi:include href="xml/calls-manager.xml"/>
       <xi:include href="xml/connectivity-info.xml"/>
       <xi:include href="xml/dbus-notification.xml"/>

src/call.h
@@ -1,0 +1,21 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+#pragma once
+
+#include "calls-dbus.h"
+
+#include <cui-call.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define PHOSH_TYPE_CALL (phosh_call_get_type())
+
+G_DECLARE_FINAL_TYPE (PhoshCall, phosh_call, PHOSH, CALL, GObject)
+
+PhoshCall * phosh_call_new                (PhoshCallsDBusCallsCall *proxy);
+
+G_END_DECLS

src/calls-manager.c
@@ -10,6 +10,7 @@
 
 #include "config.h"
 
+#include "call.h"
 #include "calls-manager.h"
 #include "shell.h"
 #include "util.h"
@@ -24,8 +25,10 @@
  * @short_description: Track ongoing phone calls
  * @Title: PhoshCallsManager
  *
- * #PhoshCallsManager tracks on going calls and allows
- * interaction with them.
+ * #PhoshCallsManager tracks on going calls on the sm.puri.Calls DBus
+ * interface and allows interaction with them by wrapping the
+ * #PhoshCallsDBusCallsCall proxies in #PhoshCall so all DBus and
+ * ObjectManager related logic stays local within #PhoshCallsManager.
  */
 
 enum {
@@ -39,6 +42,7 @@ static GParamSpec *props[PROP_LAST_PROP];
 
 enum {
   CALL_INBOUND,
+  CALL_REMOVED,
   N_SIGNALS
 };
 static guint signals[N_SIGNALS] = { 0 };
@@ -79,9 +83,13 @@ on_call_state_changed (PhoshCallsManager       *self,
 {
   const char *path;
   PhoshCallState state;
+  PhoshCall *call;
 
   g_return_if_fail (PHOSH_IS_CALLS_MANAGER (self));
   g_return_if_fail (PHOSH_CALLS_DBUS_IS_CALLS_CALL (proxy));
+
+  call = g_object_get_data (G_OBJECT (proxy), "call");
+  g_return_if_fail (call);
 
   path = g_dbus_proxy_get_object_path (G_DBUS_PROXY (proxy));
   state = phosh_calls_dbus_calls_call_get_state (proxy);
@@ -117,8 +125,8 @@ on_call_proxy_new_for_bus_finish (GObject      *source_object,
   const char *path;
   gboolean inbound;
   PhoshCallsManager *self;
-  PhoshCallsDBusCallsCall *proxy;
-
+  g_autoptr (PhoshCallsDBusCallsCall) proxy = NULL;
+  g_autoptr (PhoshCall) call = NULL;
   g_autoptr (GError) err = NULL;
 
   proxy = phosh_calls_dbus_calls_call_proxy_new_for_bus_finish (res, &err);
@@ -130,10 +138,15 @@ on_call_proxy_new_for_bus_finish (GObject      *source_object,
   self = PHOSH_CALLS_MANAGER (data);
   path = g_dbus_proxy_get_object_path (G_DBUS_PROXY (proxy));
 
+  /* Wrap DBus proxy in PhoshCall */
+  call = phosh_call_new (proxy);
+  g_object_set_data (G_OBJECT (proxy), "call", call);
+
   if (g_hash_table_contains (self->calls, path))
     g_critical ("Already got a call with path %s", path);
   else
-    g_hash_table_insert (self->calls, g_strdup (path), proxy);
+    g_hash_table_insert (self->calls, g_strdup (path), g_steal_pointer (&call));
+
 
   g_signal_connect_swapped (proxy,
                             "notify::state",
@@ -142,9 +155,10 @@ on_call_proxy_new_for_bus_finish (GObject      *source_object,
   on_call_state_changed (self, NULL, proxy);
 
   inbound = phosh_calls_dbus_calls_call_get_inbound (proxy);
-  g_debug ("Added call %s, incoming: %d", path, inbound);
+  g_debug ("Added call %s, inbound: %d", path, inbound);
+
   if (inbound)
-    g_signal_emit (self, signals[CALL_INBOUND], 0);
+    g_signal_emit (self, signals[CALL_INBOUND], 0, path);
 }
 
 
@@ -191,7 +205,8 @@ on_call_obj_removed (PhoshCallsManager *self,
   }
 
   g_debug ("Removed call %s", path);
-
+  g_signal_emit (self, signals[CALL_REMOVED], 0, path);
+  /* This disposes the call object and hence the proxy */
   g_return_if_fail (g_hash_table_remove (self->calls, path));
 }
 
@@ -368,8 +383,22 @@ phosh_calls_manager_class_init (PhoshCallsManagerClass *klass)
   g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 
   signals[CALL_INBOUND] = g_signal_new ("call-inbound",
-                                        G_TYPE_FROM_CLASS (klass), G_SIGNAL_RUN_LAST, 0, NULL, NULL,
-                                        NULL, G_TYPE_NONE, 0);
+                                        G_TYPE_FROM_CLASS (klass),
+                                        G_SIGNAL_RUN_LAST,
+                                        0, NULL, NULL,
+                                        NULL,
+                                        G_TYPE_NONE,
+                                        1,
+                                        G_TYPE_STRING);
+
+  signals[CALL_REMOVED] = g_signal_new ("call-removed",
+                                        G_TYPE_FROM_CLASS (klass),
+                                        G_SIGNAL_RUN_LAST,
+                                        0, NULL, NULL,
+                                        NULL,
+                                        G_TYPE_NONE,
+                                        1,
+                                        G_TYPE_STRING);
 }
 
 
@@ -407,9 +436,18 @@ phosh_calls_manager_get_incoming (PhoshCallsManager *self)
 
 
 const char *
-phosh_calls_manager_get_active_call (PhoshCallsManager *self)
+phosh_calls_manager_get_active_call_handle (PhoshCallsManager *self)
 {
   g_return_val_if_fail (PHOSH_IS_CALLS_MANAGER (self), NULL);
 
   return self->active_call;
+}
+
+
+PhoshCall *
+phosh_calls_manager_get_call (PhoshCallsManager *self, const char *handle)
+{
+  g_return_val_if_fail (PHOSH_IS_CALLS_MANAGER (self), NULL);
+
+  return g_hash_table_lookup (self->calls, handle);
 }

src/calls-manager.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include "call.h"
 #include "manager.h"
 
 #include <glib-object.h>
@@ -35,8 +36,9 @@ typedef enum
 G_DECLARE_FINAL_TYPE (PhoshCallsManager, phosh_calls_manager, PHOSH, CALLS_MANAGER, PhoshManager)
 
 PhoshCallsManager *phosh_calls_manager_new (void);
-gboolean phosh_calls_manager_get_present (PhoshCallsManager *self);
-gboolean phosh_calls_manager_get_incoming (PhoshCallsManager *self);
-const char *phosh_calls_manager_get_active_call (PhoshCallsManager *self);
+gboolean           phosh_calls_manager_get_present (PhoshCallsManager *self);
+gboolean           phosh_calls_manager_get_incoming (PhoshCallsManager *self);
+const char        *phosh_calls_manager_get_active_call_handle (PhoshCallsManager *self);
+PhoshCall         *phosh_calls_manager_get_call (PhoshCallsManager *self, const char *handle);
 
 G_END_DECLS

src/meson.build
@@ -57,6 +57,8 @@ libphosh_tool_sources = files(
   'app-list-model.h',
   'background.c',
   'background.h',
+  'call.c',
+  'call.h',
   'calls-manager.c',
   'calls-manager.h',
   'connectivity-info.c',

src/proximity.c
@@ -129,7 +129,7 @@ on_has_proximity_changed (PhoshProximity          *self,
   g_debug ("Found %s proximity sensor", has_proximity ? "a" : "no");
 
   /* If prox went a way we always unclaim but only claim on ongoing calls: */
-  if (!phosh_calls_manager_get_active_call (self->calls_manager) && has_proximity)
+  if (!phosh_calls_manager_get_active_call_handle (self->calls_manager) && has_proximity)
     return;
 
   phosh_proximity_claim_proximity (self, has_proximity);
@@ -146,7 +146,7 @@ on_calls_manager_active_call_changed (PhoshProximity    *self,
   g_return_if_fail (PHOSH_IS_PROXIMITY (self));
   g_return_if_fail (PHOSH_IS_CALLS_MANAGER (calls_manager));
 
-  active = !!phosh_calls_manager_get_active_call(self->calls_manager);
+  active = !!phosh_calls_manager_get_active_call_handle (self->calls_manager);
   phosh_proximity_claim_proximity (self, active);
   /* TODO: if call is over wait until we hit the threshold */
 }

commit 4bd8b5671305b4b50fd58f7ddaf89d4a357da6ae

call: Use PROP_ID to override id, not PROP_DISPLAY_NAME
And the other way around.

Fixes: 70dd1f2825d18a23337fb56dad9c469f2858bc29

Signed-off-by: Guido Günther <guido.gunther@puri.sm>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1025>

src/call.c
@@ -267,11 +267,11 @@ phosh_call_class_init (PhoshCallClass *klass)
                                     "avatar-icon");
 
   g_object_class_override_property (object_class,
-                                    PROP_DISPLAY_NAME,
+                                    PROP_ID,
                                     "id");
 
   g_object_class_override_property (object_class,
-                                    PROP_ID,
+                                    PROP_DISPLAY_NAME,
                                     "display-name");
 
   g_object_class_override_property (object_class,