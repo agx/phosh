7ab15fbeed63aed4997449cc04adbea2a8ae1e60
Guido Günther <agx@sigxcpu.org>
2022-04-05 11:31:16 +0200

    gitlab-ci: Build container without cache
    This makes sure the container is rebuilt instead of just refreshed.
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1058>

--- .gitlab-ci/run-docker.sh
+++ .gitlab-ci/run-docker.sh
@@ -106,6 +106,7 @@ if [ $build == 1 ]; then
         echo -e "\e[1;32mBUILDING\e[0m: ${base} as ${TAG}"
         ${CMD} build \
                 ${format} \
+		--no-cache \
                 --volume "$(pwd)/..:/home/user/app" \
                 --build-arg HOST_USER_ID="$UID" \
                 --tag "${TAG}" \
