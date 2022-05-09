58dfafd8875b7c12d510bc9559f2b6b369a306c4
Guido Günther <agx@sigxcpu.org>
2022-05-09 16:12:25 +0200

    build: Generate module cache on install
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1106>

--- build-aux/post_install.py
+++ build-aux/post_install.py
@@ -6,6 +6,17 @@ import sys
 
 if 'DESTDIR' not in os.environ:
     datadir = sys.argv[1]
+    phosh_plugins_dir = sys.argv[2]
 
     print('Compiling gsettings schemas...')
     subprocess.call(['glib-compile-schemas', os.path.join(datadir, 'glib-2.0', 'schemas')])
+
+    print('Updating module cache for print backends...')
+    os.makedirs(phosh_plugins_dir, exist_ok=True)
+    gio_querymodules = subprocess.check_output(['pkg-config',
+                                                '--variable=gio_querymodules',
+                                                'gio-2.0']).strip()
+    if not os.path.exists(gio_querymodules):
+        # pkg-config variables only available since GLib 2.62.0.
+        gio_querymodules = 'gio-querymodules'
+    subprocess.call([gio_querymodules, phosh_plugins_dir])
