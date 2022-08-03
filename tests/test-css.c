e964d4775da17526b1662b49acf64641e13a23c7
Guido Günther <agx@sigxcpu.org>
2022-08-03 21:20:13 +0200

    tests: Add simple test to load stylesheet
    Stylesheet changes trigger warnings ever so often, add a test
    to catch these early.
    
    $ meson test -C  _build/ --print-errorlogs css
    ...
    1/1 phosh:unit / css        FAIL            0.37s   killed by signal 5 SIGTRAP
    ...
    stderr:
    
    (/var/scratch/librem5/phosh/_build/tests/test-css:75236): Gtk-WARNING **: 21:22:54.784: Theme parsing error: common.css:513:15: Invalid name of pseudo-class
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1123>

---
+++ tests/test-css.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#include "util.h"
+
+/* Load the stylesheets to catch CSS parser warnings */
+
+static const char *
+load_theme (const char *theme_name)
+{
+  const char *style;
+  g_autofree char *name = NULL;
+  g_autoptr (GtkCssProvider) provider = gtk_css_provider_new ();
+
+  g_debug ("GTK theme: %s", theme_name);
+
+  style = phosh_util_get_stylesheet (theme_name);
+  gtk_css_provider_load_from_resource (provider, style);
+  gtk_style_context_add_provider_for_screen (gdk_screen_get_default (),
+                                             GTK_STYLE_PROVIDER (provider),
+                                             GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+  return style;
+}
+
+
+static void
+test_phosh_css_default(void)
+{
+  g_assert_cmpstr (load_theme ("Adwaita"), ==, "/sm/puri/phosh/stylesheet/adwaita-dark.css");
+}
+
+
+static void
+test_phosh_css_highcontrast(void)
+{
+  g_assert_cmpstr (load_theme ("HighContrast"), ==, "/sm/puri/phosh/stylesheet/adwaita-hc-light.css");
+}
+
+
+int
+main (int   argc,
+      char *argv[])
+{
+  gtk_test_init (&argc, &argv, NULL);
+
+  g_test_add_func("/phosh/css/default", test_phosh_css_default);
+  g_test_add_func("/phosh/css/highcontrast", test_phosh_css_highcontrast);
+  return g_test_run();
+}
