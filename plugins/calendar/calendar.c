46e5195eb89ce4f04dadcddcf3e842ddbde9b693
Chris Talbot <chris@talbothome.com>
2022-12-16 14:59:40 -0500

    lockscreen-plugins-calendar: Add CSS
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1167>

--- plugins/calendar/calendar.c
+++ plugins/calendar/calendar.c
@@ -28,10 +28,21 @@ phosh_calendar_class_init (PhoshCalendarClass *klass)
 
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/sm/puri/phosh/plugins/calendar/calendar.ui");
+
+  gtk_widget_class_set_css_name (widget_class, "phosh-calendar");
 }
 
 static void
 phosh_calendar_init (PhoshCalendar *self)
 {
+  g_autoptr (GtkCssProvider) css_provider = NULL;
+
   gtk_widget_init_template (GTK_WIDGET (self));
+
+  css_provider = gtk_css_provider_new ();
+  gtk_css_provider_load_from_resource (css_provider,
+                                       "/sm/puri/phosh/plugins/calendar/stylesheet/common.css");
+  gtk_style_context_add_provider (gtk_widget_get_style_context (GTK_WIDGET (self)),
+                                  GTK_STYLE_PROVIDER (css_provider),
+                                  GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
