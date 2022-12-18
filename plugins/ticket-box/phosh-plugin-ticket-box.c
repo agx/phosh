40c01757de0514f4c34410a8666ebe782f4ad293
Guido Günther <agx@sigxcpu.org>
2022-12-18 13:53:18 +0100

    plugins/ticket-box: Pass plugin name to compilation units
    This makes sure they don't get out of sync
    
    Signed-off-by: Guido Günther <guido.gunther@puri.sm>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1168>

--- plugins/ticket-box/phosh-plugin-ticket-box.c
+++ plugins/ticket-box/phosh-plugin-ticket-box.c
@@ -19,7 +19,7 @@ g_io_module_load (GIOModule *module)
 
   g_io_extension_point_implement (PHOSH_PLUGIN_EXTENSION_POINT_LOCKSCREEN_WIDGET,
                                   PHOSH_TYPE_TICKET_BOX,
-                                  "ticket-box",
+                                  PLUGIN_NAME,
                                   10);
 }
 
