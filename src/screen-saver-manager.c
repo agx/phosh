9d2cd6fe4425dc2a67dfa4278519dd04eb3c2385
Guido Günther <agx@sigxcpu.org>
2022-12-30 10:59:27 +0100

    screen-saver-manager: Improve debug message
    Let's debug both cases
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1178>

--- src/screen-saver-manager.c
+++ src/screen-saver-manager.c
@@ -260,6 +260,7 @@ on_logind_unlock (PhoshScreenSaverManager *self, PhoshDBusLoginSession *proxy)
   phosh_lockscreen_manager_set_locked  (self->lockscreen_manager, FALSE);
 }
 
+
 static void
 on_logind_prepare_for_sleep (PhoshScreenSaverManager *self,
                              gboolean                 suspending,
@@ -267,12 +268,12 @@ on_logind_prepare_for_sleep (PhoshScreenSaverManager *self,
 {
   g_return_if_fail (PHOSH_IS_SCREEN_SAVER_MANAGER (self));
 
+  g_debug ("Got PrepareForSleep signal: %s", suspending ? "suspend" : "resume");
   if (suspending) {
     phosh_lockscreen_manager_set_locked (self->lockscreen_manager, TRUE);
   } else {
     PhoshIdleManager *idle_manager;
 
-    g_debug ("Got PrepareForSleep signal (resume case)");
     phosh_screen_saver_manager_wakeup_screen (self);
 
     idle_manager = phosh_idle_manager_get_default ();
