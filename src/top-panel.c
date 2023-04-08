63fae83fa5ef42ade7fb898164fc7cb2d27c40a7
Guido Günther <agx@sigxcpu.org>
2023-04-08 14:21:14 +0200

    top-panel: Trigger power-menu
    Instead of the somewhat fiddly power-off/reboot/... menu use
    the more phone friendly power-button menu.
    
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1224>

--- src/top-panel.c
+++ src/top-panel.c
@@ -61,7 +61,6 @@ typedef struct _PhoshTopPanel {
 
   /* Top row above settings */
   GtkWidget *btn_power;
-  GtkWidget *menu_power;
   GtkWidget *btn_lock;
   GtkWidget *lbl_clock2;
   GtkWidget *lbl_date;
@@ -368,15 +367,6 @@ on_key_press_event (PhoshTopPanel *self, GdkEventKey *event, gpointer data)
 static void
 released_cb (PhoshTopPanel *self, int n_press, double x, double y, GtkGestureMultiPress *gesture)
 {
-  /*
-   * The popover has to be popdown manually as it doesn't happen
-   * automatically when the power button is tapped with touch
-   */
-  if (gtk_widget_is_visible (self->menu_power)) {
-    gtk_popover_popdown (GTK_POPOVER (self->menu_power));
-    return;
-  }
-
   if (phosh_util_gesture_is_touch (GTK_GESTURE_SINGLE (gesture)) == FALSE)
     g_signal_emit (self, signals[ACTIVATED], 0);
 }
@@ -445,9 +435,6 @@ on_drag_state_changed (PhoshTopPanel *self)
   const char *visible;
   gboolean kbd_interactivity = FALSE;
   double arrow = -1.0;
-
-  /* Close the popover on any drag */
-  gtk_widget_hide (self->menu_power);
 
   switch (phosh_drag_surface_get_drag_state (PHOSH_DRAG_SURFACE (self))) {
   case PHOSH_DRAG_SURFACE_STATE_UNFOLDED:
@@ -694,7 +681,6 @@ phosh_top_panel_class_init (PhoshTopPanelClass *klass)
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/sm/puri/phosh/ui/top-panel.ui");
   gtk_widget_class_bind_template_child (widget_class, PhoshTopPanel, arrow);
-  gtk_widget_class_bind_template_child (widget_class, PhoshTopPanel, menu_power);
   gtk_widget_class_bind_template_child (widget_class, PhoshTopPanel, btn_power);
   gtk_widget_class_bind_template_child (widget_class, PhoshTopPanel, btn_lock);
   gtk_widget_class_bind_template_child (widget_class, PhoshTopPanel, batteryinfo);
