bd1debc627e527048f3e5002cc4fb84e677f48b3
Guido Günther <agx@sigxcpu.org>
2023-05-05 19:45:12 +0200

    notify-feedback: Unblank screen on new notifications
    We allow to filter by urgency and category. This allows to e.g. enable
    screen wakup for all 'critical' messages and those send by alarm-clocks
    but not instant-messages via
    
    wakeup-screen-triggers == [ 'urgency', 'category' ]
    wakeup-screen-urgency == 'critical'
    wakeup-screen-categories == [ 'x-gnome.clock.alarm' ]
    
    (assuming the alarm clock sets the above category)
    
    To match on all instant messages too we'd do:
    
    wakeup-screen-categories == [ 'x-gnome.clock.alarm', 'im' ]
    
    This will match on all notifications with category class `im`
    independent from the specific. So we'd match on `im`, `im.received` and
    `im.error`.
    
    Available categories (of the form `class.specific`) can be found in the
    xdg spec:
    
    https://specifications.freedesktop.org/notification-spec/notification-spec-latest.html
    
    Closes: https://gitlab.gnome.org/World/Phosh/phosh/-/issues/934
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1244>

--- src/notifications/notify-feedback.h
+++ src/notifications/notify-feedback.h
@@ -12,10 +12,28 @@
 
 G_BEGIN_DECLS
 
+/**
+ * PhoshNotifyScreenWakeupFlags:
+ * @PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_NONE: No wakeup
+ * @PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_URGENCY: Wakeup screen based on notification urgency
+ * @PHOSH_NOTIFY_SCREEN_WAKUP_FLAG_CATEGORY: Wakeup screen based on notification category
+ * @PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_ANY: Wakeup screen on any notification
+ *
+ * What notification properties trigger screen wakeup
+ */
+typedef enum {
+  PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_NONE     = 0, /*< skip >*/
+  PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_ANY      = (1 << 0),
+  PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_URGENCY  = (1 << 1),
+  PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_CATEGORY = (1 << 2),
+} PhoshNotifyScreenWakeupFlags;
+
 #define PHOSH_TYPE_NOTIFY_FEEDBACK (phosh_notify_feedback_get_type ())
 
 G_DECLARE_FINAL_TYPE (PhoshNotifyFeedback, phosh_notify_feedback, PHOSH, NOTIFY_FEEDBACK, GObject)
 
 PhoshNotifyFeedback *phosh_notify_feedback_new (PhoshNotificationList *list);
+gboolean             phosh_notify_feedback_check_screen_wakeup (PhoshNotifyFeedback *self,
+                                                                PhoshNotification   *notification);
 
 G_END_DECLS
---
+++ tests/test-notify-feedback.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright © 2023 Guido Günther <agx@sigxcpu.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ */
+
+#include "notifications/notify-feedback.h"
+
+
+#include <gio/gdesktopappinfo.h>
+
+
+static void
+test_phosh_notify_feedback_screen_wakeup (void)
+{
+  g_autoptr (PhoshNotification) noti = NULL;
+  g_autoptr (GAppInfo) info = NULL;
+  g_autoptr (GDateTime) now = g_date_time_new_now_local ();
+  g_autoptr (PhoshNotificationList) list = phosh_notification_list_new ();
+  g_autoptr (PhoshNotifyFeedback) notify_feedback = phosh_notify_feedback_new (list);
+  g_autoptr (GSettings) settings = g_settings_new ("sm.puri.phosh.notifications");
+
+  info = G_APP_INFO (g_desktop_app_info_new ("demo.app.Second.desktop"));
+
+  noti = phosh_notification_new (0,
+                                 "should-not-be-seen",
+                                 info,
+                                 "Hey",
+                                 "Testing",
+                                 NULL,
+                                 NULL,
+                                 PHOSH_NOTIFICATION_URGENCY_NORMAL,
+                                 NULL,
+                                 FALSE,
+                                 FALSE,
+                                 NULL,
+                                 now);
+
+  g_assert_false (phosh_notify_feedback_check_screen_wakeup (notify_feedback, noti));
+
+  /* urgency */
+  g_settings_set_flags (settings, "wakeup-screen-triggers", PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_URGENCY);
+  g_settings_set_enum (settings, "wakeup-screen-urgency", PHOSH_NOTIFICATION_URGENCY_NORMAL);
+
+  g_assert_true (phosh_notify_feedback_check_screen_wakeup (notify_feedback, noti));
+
+  g_settings_set_flags (settings, "wakeup-screen-triggers", PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_URGENCY);
+  g_settings_set_enum (settings, "wakeup-screen-urgency", PHOSH_NOTIFICATION_URGENCY_NORMAL);
+
+  /* category */
+  g_settings_set_flags (settings, "wakeup-screen-triggers", PHOSH_NOTIFY_SCREEN_WAKEUP_FLAG_CATEGORY);
+  g_settings_set_strv (settings, "wakeup-screen-categories",
+                       (const char *const[]) { "im", "foo.bar", "baz" , NULL });
+
+  g_assert_false (phosh_notify_feedback_check_screen_wakeup (notify_feedback, noti));
+
+  /* Exact match without specific */
+  phosh_notification_set_category  (noti, "im");
+  g_assert_true (phosh_notify_feedback_check_screen_wakeup (notify_feedback, noti));
+
+  /* Exact match with specific but config only specifies class */
+  phosh_notification_set_category  (noti, "im.received");
+  g_assert_true (phosh_notify_feedback_check_screen_wakeup (notify_feedback, noti));
+
+  /* No match */
+  phosh_notification_set_category  (noti, "doesnotexist");
+  g_assert_false (phosh_notify_feedback_check_screen_wakeup (notify_feedback, noti));
+
+  /* Exact match with specific and config has specific too */
+  phosh_notification_set_category  (noti, "foo.bar");
+  g_assert_true (phosh_notify_feedback_check_screen_wakeup (notify_feedback, noti));
+}
+
+
+int
+main (int argc, char **argv)
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/phosh/notifyfeedback/check-screen-wakeup", test_phosh_notify_feedback_screen_wakeup);
+
+  return g_test_run ();
+}
