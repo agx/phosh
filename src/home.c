agx/phosh

commit fe308c2ce582b250e97a898cecc97d208f70bb73

home: Shrink home-bar height and move osk activation to center
Reduce the home-bar size from 40px to 15px
Replace osk button with powerbar widget and move it to the center
Modify the on pressed function to work with long press gesture
Use osk-manager directly
Add an svg image to represent the new powerbar widget
Add both powerbar and arrow into a gtk stack since both are occupying the center region
Reuse phosh_home_update_osk_button to switch home-bar stack when dragging

Closes: https:// gitlab.gnome.org/World/Phosh/phosh/-/issues/380
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1206>

src/home.c
@@ -14,7 +14,7 @@
 #include "home.h"
 #include "shell.h"
 #include "phosh-enums.h"
-#include "osk-button.h"
+#include "osk-manager.h"
 #include "util.h"
 
 #include <handy.h>
@@ -56,8 +56,11 @@ struct _PhoshHome
   PhoshDragSurface parent;
 
   GtkWidget *arrow_home;
-  GtkWidget *revealer_osk;
   GtkWidget *overview;
+  GtkWidget *powerbar;
+  PhoshOskManager *osk;
+  GtkWidget *stack;
+
   guint      debounce_handle;
   gboolean   focus_app_search;
 
@@ -71,21 +74,22 @@ struct _PhoshHome
   gboolean        osk_enabled;
 
   GtkGesture     *click_gesture; /* needed so that the gesture isn't destroyed immediately */
+  GtkGesture     *osk_toggle_long_press; /* to toggle osk from the home bar itself */
+
 };
 G_DEFINE_TYPE(PhoshHome, phosh_home, PHOSH_TYPE_DRAG_SURFACE);
 
 
 static void
-phosh_home_update_osk_button (PhoshHome *self)
+phosh_home_update_home_bar (PhoshHome *self)
 {
-  gboolean visible = FALSE;
+  const char *visible_child = "home-bar-unfolded";
   PhoshDragSurfaceState drag_state = phosh_drag_surface_get_drag_state (PHOSH_DRAG_SURFACE (self));
 
-  if (self->osk_enabled && self->state == PHOSH_HOME_STATE_FOLDED &&
-      drag_state != PHOSH_DRAG_SURFACE_STATE_DRAGGED)
-    visible = TRUE;
+  if (self->state == PHOSH_HOME_STATE_FOLDED && drag_state != PHOSH_DRAG_SURFACE_STATE_DRAGGED)
+    visible_child = "home-bar-folded";
 
-  gtk_revealer_set_reveal_child (GTK_REVEALER (self->revealer_osk), visible);
+  gtk_stack_set_visible_child_name (GTK_STACK (self->stack), visible_child);
 }
 
 
@@ -104,7 +108,6 @@ phosh_home_set_property (GObject *object,
       break;
     case PROP_OSK_ENABLED:
       self->osk_enabled = g_value_get_boolean (value);
-      phosh_home_update_osk_button (self);
       g_object_notify_by_pspec (G_OBJECT (self), props[PROP_OSK_ENABLED]);
       break;
     default:
@@ -145,6 +148,9 @@ update_drag_handle (PhoshHome *self, gboolean commit)
   gboolean arrow_visible = TRUE;
   PhoshDragSurfaceDragMode drag_mode = PHOSH_DRAG_SURFACE_DRAG_MODE_HANDLE;
   PhoshDragSurfaceState drag_state = phosh_drag_surface_get_drag_state (PHOSH_DRAG_SURFACE (self));
+
+  /* reset osk_toggle_long_press to prevent OSK from unfolding accidently */
+  gtk_event_controller_reset (GTK_EVENT_CONTROLLER (self->osk_toggle_long_press));
 
   /* Update the handle's arrow and dragability */
   if (phosh_overview_has_running_activities (PHOSH_OVERVIEW (self->overview)) == FALSE &&
@@ -215,11 +221,31 @@ on_home_released (GtkButton *button, int n_press, double x, double y, GtkGesture
 
 
 static void
-osk_clicked_cb (PhoshHome *self, GtkButton *btn)
+on_powerbar_pressed (PhoshHome *self, PhoshOskManager *osk, PhoshShell *shell)
 {
+  gboolean osk_is_available, osk_current_state, osk_new_state;
+
   g_return_if_fail (PHOSH_IS_HOME (self));
-  g_return_if_fail (GTK_IS_BUTTON (btn));
-  g_signal_emit(self, signals[OSK_ACTIVATED], 0);
+  shell = phosh_shell_get_default ();
+  self->osk = phosh_shell_get_osk_manager (shell);
+
+  osk_is_available = phosh_osk_manager_get_available (self->osk);
+  osk_current_state = phosh_osk_manager_get_visible (self->osk);
+  osk_new_state = osk_current_state;
+
+  gtk_gesture_set_state ((self->click_gesture), GTK_EVENT_SEQUENCE_DENIED);
+
+  if (osk_is_available) {
+    osk_new_state = !osk_current_state;
+  } else {
+    return;
+  }
+
+  if (osk_new_state)
+    g_signal_emit (self, signals[OSK_ACTIVATED], 0);
+
+  g_debug ("OSK toggled with pressed signal");
+  phosh_osk_manager_set_visible (self->osk, osk_new_state);
 }
 
 
@@ -426,7 +452,7 @@ on_drag_state_changed (PhoshHome *self)
     g_object_notify_by_pspec (G_OBJECT (self), props[PROP_HOME_STATE]);
   }
 
-  phosh_home_update_osk_button (self);
+  phosh_home_update_home_bar (self);
 
   phosh_layer_surface_set_kbd_interactivity (PHOSH_LAYER_SURFACE (self), kbd_interactivity);
   update_drag_handle (self, FALSE);
@@ -458,6 +484,7 @@ phosh_home_constructed (GObject *object)
   g_signal_connect (self, "notify::drag-state", G_CALLBACK (on_drag_state_changed), NULL);
 
   g_object_set_data (G_OBJECT (self->click_gesture), "phosh-home", self);
+  g_object_set_data (G_OBJECT (self->osk_toggle_long_press), "phosh-home", self);
 }
 
 
@@ -516,19 +543,20 @@ phosh_home_class_init (PhoshHomeClass *klass)
   g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 
   g_type_ensure (PHOSH_TYPE_ARROW);
-  g_type_ensure (PHOSH_TYPE_OSK_BUTTON);
   g_type_ensure (PHOSH_TYPE_OVERVIEW);
 
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/sm/puri/phosh/ui/home.ui");
   gtk_widget_class_bind_template_child (widget_class, PhoshHome, arrow_home);
-  gtk_widget_class_bind_template_child (widget_class, PhoshHome, revealer_osk);
+  gtk_widget_class_bind_template_child (widget_class, PhoshHome, powerbar);
+  gtk_widget_class_bind_template_child (widget_class, PhoshHome, stack);
   gtk_widget_class_bind_template_child (widget_class, PhoshHome, click_gesture);
+  gtk_widget_class_bind_template_child (widget_class, PhoshHome, osk_toggle_long_press);
   gtk_widget_class_bind_template_child (widget_class, PhoshHome, overview);
   gtk_widget_class_bind_template_callback (widget_class, fold_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_home_released);
   gtk_widget_class_bind_template_callback (widget_class, on_has_activities_changed);
-  gtk_widget_class_bind_template_callback (widget_class, osk_clicked_cb);
+  gtk_widget_class_bind_template_callback (widget_class, on_powerbar_pressed);
   gtk_widget_class_bind_template_callback (widget_class, window_key_press_event_cb);
 
   gtk_widget_class_set_css_name (widget_class, "phosh-home");
@@ -543,7 +571,7 @@ phosh_home_init (PhoshHome *self)
 
   gtk_widget_init_template (GTK_WIDGET (self));
 
-  phosh_home_update_osk_button (self);
+  phosh_home_update_home_bar (self);
 
   /* Adjust margins and folded state on size changes */
   g_signal_connect (self, "configure-event", G_CALLBACK (on_configure_event), NULL);

data/icons/input-powerbar-symbolic.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="150" height="15"><path d="M3.5 6h143c.831 0 1.5.669 1.5 1.5s-.669 1.5-1.5 1.5H3.5C2.669 9 2 8.331 2 7.5S2.669 6 3.5 6Z" style="fill:#fff;"/></svg>

src/home.h
@@ -12,7 +12,7 @@
 
 #define PHOSH_TYPE_HOME (phosh_home_get_type())
 
-#define PHOSH_HOME_BUTTON_HEIGHT 40
+#define PHOSH_HOME_BUTTON_HEIGHT 15
 
 /**
  * PhoshHomeState:

src/phosh.gresources.xml
@@ -52,6 +52,7 @@
     <file alias="eye-not-looking-symbolic.svg">../data/icons/eye-not-looking-symbolic.svg</file>
     <file alias="eye-open-negative-filled-symbolic.svg">../data/icons/eye-open-negative-filled-symbolic.svg</file>
     <file alias="feedback-quiet-symbolic.svg">../data/icons/feedback-quiet-symbolic.svg</file>
+    <file alias="input-powerbar-symbolic.svg">../data/icons/input-powerbar-symbolic.svg</file>
     <file alias="microphone-hardware-disabled-symbolic.svg">../data/icons/microphone-hardware-disabled-symbolic.svg</file>
     <file alias="moon-filled-symbolic.svg">../data/icons/moon-filled-symbolic.svg</file>
     <file alias="network-cellular-disabled-symbolic.svg">../data/icons/network-cellular-disabled-symbolic.svg</file>

src/stylesheet/common.css
@@ -175,6 +175,16 @@ phosh-audio-device-row image {
   font-size: 90%;
 }
 
+/* phosh-home */
+
+#powerbar {
+  background: -gtk-recolor(url('resource:///sm/puri/phosh/icons/scalable/status/input-powerbar-symbolic.svg'));
+  background-position: center;
+  background-size: 150px 15px;
+  background-repeat: no-repeat;
+  transition: 400ms ease;
+}
+
 /*
  * Overview (app grid with favories and, activities)
  */

src/ui/home.ui
@@ -12,57 +12,63 @@
         <property name="visible">True</property>
         <property name="orientation">vertical</property>
         <child>
-
-          <object class="GtkBox">
+          <object class="GtkEventBox" id="evbox_home">
             <property name="visible">True</property>
-            <property name="name">home-bar</property>
+            <property name="hexpand">True</property>
+            <!-- Otherwise search gets input focus and triggers OSK -->
+            <property name="can-focus">True</property>
             <!-- PHOSH_HOME_HEIGHT -->
-            <property name="height-request">40</property>
-            <child type="center">
-              <object class="GtkEventBox" id="evbox_home">
-                <property name="visible">True</property>
-                <property name="hexpand">True</property>
-                <!-- Otherwise search gets input focus and triggers OSK -->
-                <property name="can-focus">True</property>
-                <child>
-                  <object class="PhoshArrow" id="arrow_home">
-                    <property name="visible">True</property>
-                    <property name="valign">center</property>
-                    <property name="halign">center</property>
-                  </object>
-                </child>
-              </object>
-              <packing>
-                <property name="expand">True</property>
-              </packing>
-            </child>
+            <property name="height-request">15</property> 
             <child>
-              <object class="GtkRevealer" id="revealer_osk">
+              <object class="GtkStack" id="stack">
                 <property name="visible">True</property>
                 <property name="transition-type">crossfade</property>
-                <property name="reveal-child">False</property>
-                <property name="halign">end</property>
-                <property name="valign">center</property>
-                <property name="margin-end">6</property>
                 <child>
-                  <object class="PhoshOskButton" id="btn_osk">
+                  <object class="GtkBox" id="home_bar_folded">
                     <property name="visible">True</property>
-                    <property name="halign">end</property>
-                    <property name="valign">center</property>
-                    <signal name="clicked" handler="osk_clicked_cb" swapped="true"/>
-                    <style>
-                      <class name="phosh-osk-button"/>
-                    </style>
-                  </object>
+                    <property name="name">home-bar</property>
+                    <property name="halign">center</property>
+                    <property name="hexpand">True</property>
+                    <child>
+                      <object class="GtkEventBox" id="powerbar">
+                       <property name="can-focus">True</property>
+                       <property name="visible">True</property>
+                       <!-- PHOSH_HOME_HEIGHT -->
+                       <property name="height-request">15</property>
+                       <property name="width-request">200</property>
+                       <property name="name">powerbar</property>
+                       <property name="halign">center</property>
+                       <property name="valign">center</property>
+                      </object>
+                    </child>
+                  </object> <!-- home_bar (holding powerbar) when folded -->
+                  <packing>
+                   <property name="name">home-bar-folded</property>
+                  </packing>
                 </child>
-              </object>
-              <packing>
-                <property name="pack-type">end</property>
-              </packing>
+                <child>
+                  <object class="GtkBox" id="home_bar_unfolded">
+                    <property name="visible">True</property>
+                    <property name="name">home-bar-unfolded</property>
+                    <property name="halign">center</property>
+                    <property name="valign">center</property>
+                    <property name="hexpand">True</property>
+                    <child>
+                      <object class="PhoshArrow" id="arrow_home">
+                        <property name="visible">True</property>
+                        <property name="valign">center</property>
+                        <property name="halign">center</property>
+                      </object>
+                    </child>
+                  </object> <!-- home bar (holding arrow) when unfolded -->
+                  <packing>
+                   <property name="name">home-bar-unfolded</property>
+                  </packing>
+                </child>
+              </object><!--  stacked bar holding the parents of powerbar and arrow -->
             </child>
-          </object>
+          </object> <!-- event box for stacked  bar -->
         </child>
-
         <child>
           <object class="PhoshOverview" id="overview">
             <property name="visible">True</property>
@@ -82,5 +88,10 @@
   <object class="GtkGestureMultiPress" id="click_gesture">
     <property name="widget">evbox_home</property>
     <signal name="released" handler="on_home_released" swapped="true"/>
+  </object>
+  
+  <object class="GtkGestureLongPress" id="osk_toggle_long_press">
+    <property name="widget">powerbar</property>
+    <signal name="pressed" handler="on_powerbar_pressed" swapped="true"/>
   </object>
 </interface>

commit 2ef046426f06a217367f1315c20722d4e5fdffdd

home: Fix signature of on_powerbar_pressed
Osk manager and shell aren't passed since this is a GTK signal callback.

Fixes: fe308c2ce ("home: Shrink home-bar height and move osk activation to center")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1346>

src/home.c
@@ -251,7 +251,7 @@ on_powerbar_action_failed (PhoshHome *self)
 
 
 static void
-on_powerbar_pressed (PhoshHome *self, PhoshOskManager *osk, PhoshShell *shell)
+on_powerbar_pressed (PhoshHome *self)
 {
   PhoshOskManager *osk;
   gboolean osk_is_available, osk_current_state, osk_new_state;