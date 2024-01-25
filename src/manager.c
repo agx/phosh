17b351aa75067c991163b3d65c699a1327fd45a3
Guido Günther <agx@sigxcpu.org>
2024-01-25 12:26:44 +0100

    manager: Set name for idle callback
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1357>

--- src/manager.c
+++ src/manager.c
@@ -63,8 +63,10 @@ phosh_manager_constructed (GObject *object)
 
   G_OBJECT_CLASS (phosh_manager_parent_class)->constructed (object);
 
-  if (klass->idle_init)
+  if (klass->idle_init) {
     priv->idle_id = g_idle_add ((GSourceFunc) on_idle, self);
+    g_source_set_name_by_id (priv->idle_id, "[PhoshManager] idle");
+  }
 }
 
 
