179aad97849eee687116db2af128528ff0d725c6
Guido Günther <agx@sigxcpu.org>
2024-02-15 10:29:37 +0100

    screen-saver-manager: Use g_timeout_add_seconds_once
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1371>

--- src/screen-saver-manager.c
+++ src/screen-saver-manager.c
@@ -105,7 +105,7 @@ G_DEFINE_TYPE_WITH_CODE (PhoshScreenSaverManager,
                            phosh_screen_saver_manager_screen_saver_iface_init));
 
 
-static gboolean
+static void
 on_lock_delay_timer_expired (gpointer data)
 {
   PhoshScreenSaverManager *self = PHOSH_SCREEN_SAVER_MANAGER (data);
@@ -113,7 +113,6 @@ on_lock_delay_timer_expired (gpointer data)
   phosh_lockscreen_manager_set_locked (self->lockscreen_manager, TRUE);
 
   self->lock_delay_timer_id = 0;
-  return G_SOURCE_REMOVE;
 }
 
 
@@ -149,9 +148,9 @@ arm_lock_delay_timer (PhoshScreenSaverManager *self, gboolean active, gboolean l
     return;
 
   g_debug ("Arming lock delay timer for %d seconds", self->lock_delay);
-  self->lock_delay_timer_id = g_timeout_add_seconds (self->lock_delay,
-                                                     on_lock_delay_timer_expired,
-                                                     self);
+  self->lock_delay_timer_id = g_timeout_add_seconds_once (self->lock_delay,
+                                                          on_lock_delay_timer_expired,
+                                                          self);
   g_source_set_name_by_id (self->lock_delay_timer_id, "[phosh] lock_delay_timer");
 }
 
