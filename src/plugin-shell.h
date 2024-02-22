505a5c8cb848170e8b19a69ee2646c3fb587a54e
Guido Günther <agx@sigxcpu.org>
2024-02-22 18:28:19 +0100

    shell: Split out methods available to plugins
    shell.h pulls in about almost all object's headers. In order to not leak
    all of those to the plugins (requiring them to declare dependencies on
    other libraries used by those headers like NM, gnome-desktop, etc)
    introduce a separate header file that can be used by plugins to only get
    a basic set.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1373>

---
+++ src/plugin-shell.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2024 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#pragma once
+
+#include "session-manager.h"
+
+G_BEGIN_DECLS
+
+#define PHOSH_TYPE_SHELL phosh_shell_get_type ()
+
+G_DECLARE_FINAL_TYPE (PhoshShell, phosh_shell, PHOSH, SHELL, GObject)
+
+PhoshShell             *phosh_shell_get_default (void);
+
+/* Created by the shell on startup */
+PhoshSessionManager    *phosh_shell_get_session_manager (PhoshShell *self);
+
+G_END_DECLS
