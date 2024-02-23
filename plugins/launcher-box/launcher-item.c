9d168ea2155c8557a49343f860e9dfb357659b4e
Guido Günther <agx@sigxcpu.org>
2024-02-23 21:41:49 +0100

    launcher-item: Add properties to track progress and count
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1377>

--- plugins/launcher-box/launcher-item.c
+++ plugins/launcher-box/launcher-item.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Guido Günther <agx@sigxcpu.org>
+ * Copyright (C) 2024 The Phosh Developers
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
@@ -19,6 +19,10 @@
 enum {
   PROP_0,
   PROP_APP_INFO,
+  PROP_PROGRESS_VISIBLE,
+  PROP_PROGRESS,
+  PROP_COUNT_VISIBLE,
+  PROP_COUNT,
   PROP_LAST_PROP
 };
 static GParamSpec *props[PROP_LAST_PROP];
@@ -27,6 +31,10 @@ struct _PhoshLauncherItem {
   GObject               parent;
 
   GDesktopAppInfo      *app_info;
+  gboolean              progress_visible;
+  double                progress;
+  gboolean              count_visible;
+  gint64                count;
 };
 G_DEFINE_TYPE (PhoshLauncherItem, phosh_launcher_item, G_TYPE_OBJECT)
 
@@ -42,6 +50,18 @@ phosh_launcher_item_set_property (GObject      *object,
   switch (property_id) {
   case PROP_APP_INFO:
     g_set_object (&self->app_info, g_value_get_object (value));
+    break;
+  case PROP_PROGRESS_VISIBLE:
+    phosh_launcher_item_set_progress_visible (self, g_value_get_boolean (value));
+    break;
+  case PROP_PROGRESS:
+    phosh_launcher_item_set_progress (self, g_value_get_double (value));
+    break;
+  case PROP_COUNT_VISIBLE:
+    phosh_launcher_item_set_count_visible (self, g_value_get_boolean (value));
+    break;
+  case PROP_COUNT:
+    phosh_launcher_item_set_count (self, g_value_get_int64 (value));
     break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
@@ -61,6 +81,18 @@ phosh_launcher_item_get_property (GObject    *object,
   switch (property_id) {
   case PROP_APP_INFO:
     g_value_set_object (value, self->app_info);
+    break;
+  case PROP_PROGRESS_VISIBLE:
+    g_value_set_boolean (value, phosh_launcher_item_get_progress_visible (self));
+    break;
+  case PROP_PROGRESS:
+    g_value_set_double (value, phosh_launcher_item_get_progress (self));
+    break;
+  case PROP_COUNT_VISIBLE:
+    g_value_set_boolean (value, phosh_launcher_item_get_count_visible (self));
+    break;
+  case PROP_COUNT:
+    g_value_set_int64 (value, phosh_launcher_item_get_count (self));
     break;
   default:
     G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
@@ -94,6 +126,26 @@ phosh_launcher_item_class_init (PhoshLauncherItemClass *klass)
                          G_TYPE_DESKTOP_APP_INFO,
                          G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS);
 
+  props[PROP_PROGRESS_VISIBLE] =
+    g_param_spec_boolean ("progress-visible", "", "",
+                          FALSE,
+                          G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
+  props[PROP_PROGRESS] =
+    g_param_spec_double ("progress", "", "",
+                         0.0, 1.0, 0.0,
+                         G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
+  props[PROP_COUNT_VISIBLE] =
+    g_param_spec_boolean ("count-visible", "", "",
+                          FALSE,
+                          G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
+  props[PROP_COUNT] =
+    g_param_spec_int64 ("count", "", "",
+                        0, G_MAXINT64, G_MAXINT64,
+                        G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
   g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 }
 
@@ -108,6 +160,94 @@ PhoshLauncherItem *
 phosh_launcher_item_new (GDesktopAppInfo *app_info)
 {
   return g_object_new (PHOSH_TYPE_LAUNCHER_ITEM, "app-info", app_info, NULL);
+}
+
+
+gboolean
+phosh_launcher_item_get_progress_visible (PhoshLauncherItem *self)
+{
+  g_return_val_if_fail (PHOSH_IS_LAUNCHER_ITEM (self), FALSE);
+
+  return self->progress_visible;
+}
+
+
+void
+phosh_launcher_item_set_progress_visible (PhoshLauncherItem *self, gboolean visible)
+{
+  g_return_if_fail (PHOSH_IS_LAUNCHER_ITEM (self));
+
+  if (self->progress_visible == visible)
+    return;
+
+  self->progress_visible = visible;
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_PROGRESS_VISIBLE]);
+}
+
+
+double
+phosh_launcher_item_get_progress (PhoshLauncherItem *self)
+{
+  g_return_val_if_fail (PHOSH_IS_LAUNCHER_ITEM (self), 0.0);
+
+  return self->progress;
+}
+
+
+void
+phosh_launcher_item_set_progress (PhoshLauncherItem *self, double progress)
+{
+  g_return_if_fail (PHOSH_IS_LAUNCHER_ITEM (self));
+
+  if (G_APPROX_VALUE (self->progress, progress, FLT_EPSILON))
+    return;
+
+  self->progress = progress;
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_PROGRESS]);
+}
+
+
+gboolean
+phosh_launcher_item_get_count_visible (PhoshLauncherItem *self)
+{
+  g_return_val_if_fail (PHOSH_IS_LAUNCHER_ITEM (self), FALSE);
+
+  return self->count_visible;
+}
+
+
+void
+phosh_launcher_item_set_count_visible (PhoshLauncherItem *self, gboolean visible)
+{
+  g_return_if_fail (PHOSH_IS_LAUNCHER_ITEM (self));
+
+  if (self->count_visible == visible)
+    return;
+
+  self->count_visible = visible;
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_COUNT_VISIBLE]);
+}
+
+
+void
+phosh_launcher_item_set_count (PhoshLauncherItem *self, gint64 count)
+{
+  g_return_if_fail (PHOSH_IS_LAUNCHER_ITEM (self));
+
+  if (count == self->count)
+    return;
+
+  self->count = count;
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_COUNT]);
+}
+
+
+gint64
+phosh_launcher_item_get_count (PhoshLauncherItem *self)
+{
+  g_return_val_if_fail (PHOSH_IS_LAUNCHER_ITEM (self), 0);
+
+  return self->count;
 }
 
 
