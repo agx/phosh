agx/phosh

commit f228ca6e18387c6c429602732da0f234c62e60d5

treewide: Use consistent Wi-Fi spelling
Use the official spelling. See previous commit for details.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1364>

src/suspend-manager.c
@@ -77,7 +77,7 @@ on_is_hotspot_master_changed (PhoshSuspendManager *self,
   if (is_hotspot_master) {
     inhibit_suspend (self, "wifi-hotspot", "WiFi hotspot active");
   } else {
-    g_debug ("Clearing wifi hotspot suspend inhibit");
+    g_debug ("Clearing Wi-Fi hotspot suspend inhibit");
     uninhibit_suspend (self, "wifi-hotspot");
   }
 }

src/network-auth-prompt.c
@@ -199,12 +199,12 @@ network_prompt_setup_wifi_dialog (PhoshNetworkAuthPrompt *self)
     g_debug ("Network security method %d of %s not supported",
              self->security_type, ssid);
     str =  g_strdup_printf(_(
-      "Authentication type of wifi network “%s” not supported"), ssid);
+      "Authentication type of Wi-Fi network “%s” not supported"), ssid);
     gtk_label_set_label (GTK_LABEL (self->message_label), str);
     return;
   }
 
-  str = g_strdup_printf (_("Enter password for the wifi network “%s”"), ssid);
+  str = g_strdup_printf (_("Enter password for the Wi-Fi network “%s”"), ssid);
   gtk_label_set_label (GTK_LABEL (self->message_label), str);
 
   network_prompt_set_grid (self, self->wpa_grid);

src/wifi-network.c
@@ -13,7 +13,7 @@
 /**
  * PhoshWifiNetwork:
  *
- * An object that represents a WiFi network.
+ * An object that represents a Wi-Fi network.
  *
  * A network is identified by it's SSID and encryption type and mode
  */

src/wifi-status-page.c
@@ -13,7 +13,7 @@
 /**
  * PhoshWifiStatusPage:
  *
- * A widget to display list of WiFi access points when the corresponding PhoshQuickSetting is
+ * A widget to display list of Wi-Fi access points when the corresponding PhoshQuickSetting is
  * activated.
  */
 
@@ -136,7 +136,7 @@ phosh_wifi_status_page_init (PhoshWifiStatusPage *self)
   self->wifi = g_object_ref (phosh_shell_get_wifi_manager (shell));
 
   if (self->wifi == NULL) {
-    g_warning ("Failed to get WiFi manager");
+    g_warning ("Failed to get Wi-Fi manager");
     return;
   }
 

src/wifiinfo.c
@@ -6,7 +6,7 @@
  * Author: Guido Günther <agx@sigxcpu.org>
  */
 
-/* Wifi Info widget */
+/* Wi-Fi Info widget */
 
 #define G_LOG_DOMAIN "phosh-wifiinfo"
 
@@ -18,9 +18,9 @@
 /**
  * PhoshWifiInfo:
  *
- * A widget to display the wifi status
+ * A widget to display the Wi-Fi status
  *
- * #PhoshWifiInfo displays the current wifi status based on information
+ * #PhoshWifiInfo displays the current Wi-Fi status based on information
  * from #PhoshWifiManager. To figure out if the widget should be shown
  * the #PhoshWifiInfo:enabled property can be useful.
  */
@@ -68,7 +68,7 @@ update_icon (PhoshWifiInfo *self, GParamSpec *pspec, PhoshWifiManager *wifi)
 {
   const char *icon_name;
 
-  g_debug ("Updating wifi icon");
+  g_debug ("Updating Wi-Fi icon");
   g_return_if_fail (PHOSH_IS_WIFI_INFO (self));
   g_return_if_fail (PHOSH_IS_WIFI_MANAGER (wifi));
 
@@ -95,7 +95,7 @@ on_wifi_enabled (PhoshWifiInfo *self, GParamSpec *pspec, PhoshWifiManager *wifi)
 {
   gboolean enabled;
 
-  g_debug ("Updating wifi status");
+  g_debug ("Updating Wi-Fi status");
   g_return_if_fail (PHOSH_IS_WIFI_INFO (self));
   g_return_if_fail (PHOSH_IS_WIFI_MANAGER (wifi));
 
@@ -147,7 +147,7 @@ phosh_wifi_info_constructed (GObject *object)
   self->wifi = g_object_ref(phosh_shell_get_wifi_manager (shell));
 
   if (self->wifi == NULL) {
-    g_warning ("Failed to get wifi manager");
+    g_warning ("Failed to get Wi-Fi manager");
     return;
   }
 
@@ -205,18 +205,24 @@ phosh_wifi_info_class_init (PhoshWifiInfoClass *klass)
 
   gtk_widget_class_set_css_name (widget_class, "phosh-wifi-info");
 
+  /**
+   * PhoshWifiInfo:enabled:
+   *
+   * Whether a Wi-Fi device is enabled
+   */
   props[PROP_ENABLED] =
-    g_param_spec_boolean ("enabled",
-                          "enabled",
-                          "Whether a wifi device is enabled",
+    g_param_spec_boolean ("enabled", "", "",
                           FALSE,
                           G_PARAM_READABLE |
                           G_PARAM_STATIC_STRINGS |
                           G_PARAM_EXPLICIT_NOTIFY);
+  /**
+   * PhoshWifiInfo:enabled:
+   *
+   * Whether a Wi-Fi hardware is present
+   */
   props[PROP_PRESENT] =
-    g_param_spec_boolean ("present",
-                          "Present",
-                          "Whether wifi hardware is present",
+    g_param_spec_boolean ("present", "", "",
                           FALSE,
                           G_PARAM_READABLE |
                           G_PARAM_STATIC_STRINGS |

commit 79d31e4b7db0fea8e30e98157099c6489bbad664

suspend-manager: Use consistent Wi-Fi spelling
Fixes: f228ca6e1 ("treewide: Use consistent Wi-Fi spelling")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1373>

src/suspend-manager.c
@@ -75,7 +75,7 @@ on_is_hotspot_master_changed (PhoshSuspendManager *self,
   is_hotspot_master = phosh_wifi_manager_is_hotspot_master (wifi_manager);
 
   if (is_hotspot_master) {
-    inhibit_suspend (self, "wifi-hotspot", "WiFi hotspot active");
+    inhibit_suspend (self, "wifi-hotspot", "Wi-Fi hotspot active");
   } else {
     g_debug ("Clearing Wi-Fi hotspot suspend inhibit");
     uninhibit_suspend (self, "wifi-hotspot");