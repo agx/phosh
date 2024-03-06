agx/phosh

commit 1a2c6dbb1d7743984402e489d993be4c81b4ea51

screen-saver-manager: Split arming the lock delay timer into separate function
There are cases where we only want to activate the timer since the
screen is already blanked (e.g. when the compositor initiated the screen
blank).

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1207>

src/screen-saver-manager.c
@@ -114,19 +114,9 @@ unarm_lock_delay_timer (PhoshScreenSaverManager *self, const char *reason)
 }
 
 
-/* Activate or deactivate screen blank based on `active`. If `lock` is %TRUE locking
-   is also enabled (honoring the configure delay */
 static void
-screen_saver_set_active (PhoshScreenSaverManager *self, gboolean active, gboolean lock)
+arm_lock_delay_timer (PhoshScreenSaverManager *self, gboolean active, gboolean lock)
 {
-  if (self->active == active)
-    return;
-
-  g_debug ("Activiting screen saver: %d, lock: %d, lock_delay: %d", active, lock,
-    self->lock_delay);
-
-  phosh_shell_enable_power_save (phosh_shell_get_default (), active);
-
   if (!active || !lock) {
     unarm_lock_delay_timer (self, "arm");
     return;
@@ -152,6 +142,26 @@ screen_saver_set_active (PhoshScreenSaverManager *self, gboolean active, gboolea
                                                      on_lock_delay_timer_expired,
                                                      self);
   g_source_set_name_by_id (self->lock_delay_timer_id, "[phosh] lock_delay_timer");
+}
+
+
+/* Activate or deactivate screen blank based on `active`. If `lock` is %TRUE locking
+   is also enabled (honoring the configured delay) */
+static void
+screen_saver_set_active (PhoshScreenSaverManager *self, gboolean active, gboolean lock)
+{
+  if (self->active == active)
+    return;
+
+  g_debug ("Activiting screen saver: %d, lock: %d, lock_delay: %d", active, lock,
+    self->lock_delay);
+
+  /* on_primary_monitor_power_mode_changed will update self->active once the power mode is set  */
+  phosh_shell_enable_power_save (phosh_shell_get_default (), active);
+
+  /* Don't wait for on_primary_monitor_power_mode_changed to activate the lock delay timer in
+     case that fails on the compositor side - we don't want to miss the screen lock */
+  arm_lock_delay_timer (self, active, lock);
 }
 
 

commit 62a171607503a14b0aca76ac18d572fa658e0e7b

screen-saver-manager: Use lock-enabled in more places
Except in cases where the lock is requested explicity (like the DBus
call) we want to take the lock-enabled setting into account.

This fixes the case where lock-enabled is false but a lock-delay is set.
We don't want to lock in that case either.

Fixes: 1a2c6dbb1 ("screen-saver-manager: Split arming the lock delay timer into separate function")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1371>

src/screen-saver-manager.c
@@ -227,7 +227,7 @@ on_power_button_pressed (GSimpleAction *action, GVariant *param, gpointer data)
     return;
 
   g_debug ("Power button released, activating screensaver");
-  screen_saver_set_active (self, TRUE, TRUE);
+  screen_saver_set_active (self, TRUE, self->lock_enabled);
 
   /* Disable long press timer */
   g_clear_handle_id (&self->long_press_id, g_source_remove);
@@ -796,7 +796,7 @@ on_primary_monitor_power_mode_changed (PhoshScreenSaverManager *self,
   }
 
   if (active) {
-    arm_lock_delay_timer (self, active, TRUE);
+    arm_lock_delay_timer (self, active, self->lock_enabled);
   } else {
     unarm_lock_delay_timer (self, "power mode change");
   }