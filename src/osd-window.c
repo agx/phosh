agx/phosh

commit 12983abe17c1819232062436d2ca8a7ec84eee6c

Add PhoshOsdWindow
This finally adds OSD information.

Closes: #33

Signed-off-by: Guido Günther <guido.gunther@puri.sm>

src/osd-window.c
@@ -1,0 +1,229 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#define G_LOG_DOMAIN "phosh-osd-window"
+
+#include "config.h"
+
+#include "osd-window.h"
+
+/**
+ * SECTION:osd-window
+ * @short_description: A OSD Window
+ * @Title: PhoshOsdWindow
+ *
+ * The #PhoshOsdWindow displays contents fed via the
+ * OSD (on screen display) DBus interface.
+ */
+
+enum {
+  PROP_0,
+  PROP_CONNECTOR,
+  PROP_LABEL,
+  PROP_ICON_NAME,
+  PROP_LEVEL,
+  PROP_MAX_LEVEL,
+  PROP_LAST_PROP,
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+typedef struct _PhoshOsdWindow {
+  PhoshSystemModal parent;
+
+  char            *connector;
+  char            *label;
+  char            *icon_name;
+  gdouble          level;
+  gdouble          max_level;
+
+  GtkWidget       *lbl;
+  GtkWidget       *icon;
+  GtkWidget       *bar;
+  GtkGesture      *click_gesture;
+} PhoshOsdWindow;
+
+
+G_DEFINE_TYPE (PhoshOsdWindow, phosh_osd_window, PHOSH_TYPE_SYSTEM_MODAL)
+
+static void
+phosh_osd_window_set_property (GObject      *obj,
+                               guint         prop_id,
+                               const GValue *value,
+                               GParamSpec   *pspec)
+{
+  PhoshOsdWindow *self = PHOSH_OSD_WINDOW (obj);
+
+  switch (prop_id) {
+  case PROP_CONNECTOR:
+    g_free (self->connector);
+    self->connector = g_value_dup_string (value);
+    break;
+  case PROP_LABEL:
+    g_free (self->label);
+    self->label = g_value_dup_string (value);
+    gtk_label_set_label (GTK_LABEL (self->lbl), self->label);
+    break;
+  case PROP_ICON_NAME:
+    g_free (self->icon_name);
+    self->icon_name = g_value_dup_string (value);
+    gtk_image_set_from_icon_name (GTK_IMAGE (self->icon), self->icon_name, GTK_ICON_SIZE_INVALID);
+    break;
+  case PROP_LEVEL:
+    self->level = g_value_get_double (value);
+    gtk_level_bar_set_value (GTK_LEVEL_BAR (self->bar), self->level);
+    break;
+  case PROP_MAX_LEVEL:
+    self->max_level = g_value_get_double (value);
+    gtk_level_bar_set_max_value (GTK_LEVEL_BAR (self->bar), self->max_level);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, prop_id, pspec);
+    break;
+  }
+}
+
+
+static void
+phosh_osd_window_get_property (GObject    *obj,
+                               guint       prop_id,
+                               GValue     *value,
+                               GParamSpec *pspec)
+{
+  PhoshOsdWindow *self = PHOSH_OSD_WINDOW (obj);
+
+  switch (prop_id) {
+  case PROP_CONNECTOR:
+    g_value_set_string (value, self->connector ? self->connector : "");
+    break;
+  case PROP_LABEL:
+    g_value_set_string (value, self->label ? self->label : "");
+    break;
+  case PROP_ICON_NAME:
+    g_value_set_string (value, self->icon_name ? self->icon_name : "");
+    break;
+  case PROP_LEVEL:
+    g_value_set_double (value, self->level);
+    break;
+  case PROP_MAX_LEVEL:
+    g_value_set_double (value, self->max_level);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, prop_id, pspec);
+    break;
+  }
+}
+
+
+static void
+on_button_released (PhoshOsdWindow *self)
+{
+  gtk_widget_destroy (GTK_WIDGET (self));
+}
+
+
+static void
+phosh_osd_window_finalize (GObject *obj)
+{
+  PhoshOsdWindow *self = PHOSH_OSD_WINDOW (obj);
+
+  g_free (self->connector);
+  g_free (self->label);
+  g_free (self->icon_name);
+
+  G_OBJECT_CLASS (phosh_osd_window_parent_class)->finalize (obj);
+}
+
+
+static void
+phosh_osd_window_class_init (PhoshOsdWindowClass *klass)
+{
+  GObjectClass *object_class = (GObjectClass *)klass;
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->get_property = phosh_osd_window_get_property;
+  object_class->set_property = phosh_osd_window_set_property;
+  object_class->finalize = phosh_osd_window_finalize;
+
+  /* TODO: currently unused */
+  props[PROP_CONNECTOR] =
+    g_param_spec_string ("connector",
+                         "Connector",
+                         "Connector to use for osd display",
+                         NULL,
+                         G_PARAM_READWRITE);
+
+  props[PROP_LABEL] =
+    g_param_spec_string ("label",
+                         "Label",
+                         "Label to show on osd",
+                         NULL,
+                         G_PARAM_READWRITE);
+
+  props[PROP_ICON_NAME] =
+    g_param_spec_string ("icon-name",
+                         "Icon Name",
+                         "Name of icon to use on osd",
+                         NULL,
+                         G_PARAM_READWRITE);
+
+  props[PROP_LEVEL] =
+    g_param_spec_double ("level",
+                         "Level",
+                         "Level of bar to display on osd",
+                         0.0,
+                         G_MAXDOUBLE,
+                         0.0,
+                         G_PARAM_READWRITE);
+
+  props[PROP_MAX_LEVEL] =
+    g_param_spec_double ("max-level",
+                         "Maximum Level",
+                         "Maximum level of bar to display on osd",
+                         0.0,
+                         G_MAXDOUBLE,
+                         0.0,
+                         G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/sm/puri/phosh/ui/osd-window.ui");
+  gtk_widget_class_bind_template_child (widget_class, PhoshOsdWindow, lbl);
+  gtk_widget_class_bind_template_child (widget_class, PhoshOsdWindow, icon);
+  gtk_widget_class_bind_template_child (widget_class, PhoshOsdWindow, bar);
+  gtk_widget_class_bind_template_child (widget_class, PhoshOsdWindow, click_gesture);
+  gtk_widget_class_bind_template_callback (widget_class, on_button_released);
+
+  gtk_widget_class_set_css_name (widget_class, "phosh-osd-window");
+}
+
+
+static void
+phosh_osd_window_init (PhoshOsdWindow *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  gtk_widget_add_events (GTK_WIDGET (self), GDK_BUTTON_RELEASE_MASK);
+}
+
+
+GtkWidget *
+phosh_osd_window_new (char  *connector,
+                      char  *label,
+                      char  *icon_name,
+                      double level,
+                      double max_level)
+{
+  return g_object_new (PHOSH_TYPE_OSD_WINDOW,
+                       "connector", connector,
+                       "label",label,
+                       "icon-name", icon_name,
+                       "level", level,
+                       "max-level", max_level,
+                       NULL);
+}

docs/phosh-docs.xml
@@ -86,6 +86,7 @@
       <xi:include href="xml/osk-button.xml"/>
       <xi:include href="xml/osk-manager.xml"/>
       <xi:include href="xml/overview.xml"/>
+      <xi:include href="xml/osd-window.xml"/>
       <xi:include href="xml/panel.xml"/>
       <xi:include href="xml/phosh-wayland.xml"/>
       <xi:include href="xml/phosh-wwan-backend.xml"/>

src/gnome-shell-manager.c
@@ -12,7 +12,9 @@
 #include "../config.h"
 
 #include "gnome-shell-manager.h"
+#include "osd-window.h"
 #include "shell.h"
+#include "util.h"
 #include "lockscreen-manager.h"
 
 /**
@@ -23,7 +25,7 @@
  */
 
 #define NOTIFY_DBUS_NAME "org.gnome.Shell"
-
+#define OSD_HIDE_TIMEOUT 1 /* seconds */
 
 static void phosh_gnome_shell_manager_shell_iface_init (PhoshGnomeShellDBusShellIface *iface);
 
@@ -33,6 +35,10 @@ typedef struct _PhoshGnomeShellManager {
   GHashTable                      *info_by_action;
   guint                            last_action_id;
   int                              dbus_name_id;
+
+  PhoshOsdWindow                  *osd;
+  gint                             osd_timeoutid;
+  gboolean                         osd_continue;
 } PhoshGnomeShellManager;
 
 G_DEFINE_TYPE_WITH_CODE (PhoshGnomeShellManager,
@@ -110,14 +116,37 @@ handle_hide_monitor_labels (PhoshGnomeShellDBusShell *skeleton,
 
 
 static gboolean
+on_osd_timeout (PhoshGnomeShellManager *self)
+{
+  gboolean ret;
+  ret = self->osd_continue ? G_SOURCE_CONTINUE : G_SOURCE_REMOVE;
+  if (!self->osd_continue) {
+      g_debug ("Closing osd");
+      self->osd_timeoutid = 0;
+      if (self->osd)
+        gtk_widget_destroy (GTK_WIDGET (self->osd));
+  }
+  self->osd_continue = FALSE;
+  return ret;
+}
+
+static void
+on_osd_destroyed (PhoshGnomeShellManager *self)
+{
+  self->osd = NULL;
+  g_clear_handle_id (&self->osd_timeoutid, g_source_remove);
+}
+
+
+static gboolean
 handle_show_osd (PhoshGnomeShellDBusShell *skeleton,
                  GDBusMethodInvocation    *invocation,
                  GVariant                 *arg_params)
 {
   PhoshGnomeShellManager *self = PHOSH_GNOME_SHELL_MANAGER (skeleton);
   GVariantDict dict;
-  g_autofree gchar *connector = NULL, *icon = NULL, *label = NULL;
-  gdouble level = 0.0, maxlevel = 0.0;
+  g_autofree char *connector = NULL, *icon = NULL, *label = NULL;
+  gdouble level = 0.0, maxlevel = 1.0;
 
   g_return_val_if_fail (PHOSH_IS_GNOME_SHELL_MANAGER (self), FALSE);
 
@@ -126,10 +155,32 @@ handle_show_osd (PhoshGnomeShellDBusShell *skeleton,
   g_variant_dict_lookup (&dict, "icon", "s", &icon);
   g_variant_dict_lookup (&dict, "label", "s", &label);
   g_variant_dict_lookup (&dict, "level", "d", &level);
-  g_variant_dict_lookup (&dict, "maxlevel", "d", &maxlevel);
+  g_variant_dict_lookup (&dict, "max_level", "d", &maxlevel);
 
   g_debug ("DBus show osd: connector: %s icon: %s, label: %s, level %f/%f",
            connector, icon, label, level, maxlevel);
+
+  if (self->osd) {
+    self->osd_continue = TRUE;
+    g_object_set (self->osd,
+                  "connector", connector,
+                  "label", label,
+                  "icon-name", icon,
+                  "level", level,
+                  "max-level", maxlevel,
+                  NULL);
+  } else {
+    self->osd = PHOSH_OSD_WINDOW (phosh_osd_window_new (connector, label, icon, level, maxlevel));
+    g_signal_connect_swapped (self->osd, "destroy", G_CALLBACK (on_osd_destroyed), self);
+    gtk_widget_show (GTK_WIDGET (self->osd));
+  }
+
+  if (!self->osd_timeoutid) {
+    self->osd_timeoutid = g_timeout_add_seconds (OSD_HIDE_TIMEOUT,
+                                                 (GSourceFunc)on_osd_timeout,
+                                                 self);
+    g_source_set_name_by_id (self->osd_timeoutid, "[phosh] osd-timeout");
+  }
 
   phosh_gnome_shell_dbus_shell_complete_show_osd (
     skeleton, invocation);

src/meson.build
@@ -87,6 +87,8 @@ libphosh_tool_sources = [
   'mount-manager.h',
   'overview.c',
   'overview.h',
+  'osd-window.c',
+  'osd-window.h',
   'status-icon.c',
   'status-icon.h',
   'system-modal.c',

src/osd-window.h
@@ -1,0 +1,20 @@
+/*
+ * Copyright (C) 2021 Purism SPC
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+#include "system-modal.h"
+
+#define PHOSH_TYPE_OSD_WINDOW (phosh_osd_window_get_type ())
+
+G_DECLARE_FINAL_TYPE (PhoshOsdWindow, phosh_osd_window, PHOSH, OSD_WINDOW, PhoshSystemModal)
+
+GtkWidget *phosh_osd_window_new (char *connector,
+                                 char *label,
+                                 char *icon_name,
+                                 double level,
+                                 double max_level);

src/phosh.gresources.xml
@@ -12,6 +12,7 @@
     <file preprocess="xml-stripblanks">ui/notification-frame.ui</file>
     <file preprocess="xml-stripblanks">ui/polkit-auth-prompt.ui</file>
     <file preprocess="xml-stripblanks">ui/network-auth-prompt.ui</file>
+    <file preprocess="xml-stripblanks">ui/osd-window.ui</file>
     <file preprocess="xml-stripblanks">ui/settings-menu.ui</file>
     <file preprocess="xml-stripblanks">ui/system-prompt.ui</file>
     <file preprocess="xml-stripblanks">ui/top-panel.ui</file>

src/style.css
@@ -365,3 +365,30 @@ phosh-media-player box *:disabled {
   color: white;
   -gtk-icon-effect: none;
 }
+
+/*
+ * OSD
+ */
+phosh-osd-window levelbar block {
+    min-height: 16px;
+    box-shadow: none;
+    border-color: white;
+    border-radius: 5px;
+}
+
+phosh-osd-window levelbar block.low {
+  background-color: white;
+}
+
+phosh-osd-window levelbar block.high {
+  background-color: white;
+}
+
+phosh-osd-window levelbar block.full {
+  background-color: white;
+}
+
+phosh-osd-window levelbar trough {
+    box-shadow: none;
+    border-width: 0px;
+}

src/ui/osd-window.ui
@@ -1,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Generated with glade 3.38.2 -->
+<interface>
+  <requires lib="gtk+" version="3.24"/>
+  <template class="PhoshOsdWindow" parent="PhoshSystemModal">
+    <property name="can-focus">False</property>
+    <child>
+      <object class="GtkBox">
+        <property name="width-request">250</property>
+        <property name="visible">True</property>
+        <property name="can-focus">False</property>
+        <property name="halign">center</property>
+        <property name="valign">center</property>
+        <property name="orientation">vertical</property>
+        <child>
+          <object class="GtkLabel" id="lbl">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="wrap">True</property>
+            <property name="wrap-mode">word</property>
+            <attributes>
+              <attribute name="scale" value="3"/>
+            </attributes>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">0</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkImage" id="icon">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-top">20</property>
+            <property name="stock">gtk-missing-image</property>
+            <property name="pixel-size">100</property>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">1</property>
+          </packing>
+        </child>
+        <child>
+          <object class="GtkLevelBar" id="bar">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-top">20</property>
+            <property name="min-value">0.0</property>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">2</property>
+          </packing>
+        </child>
+      </object>
+    </child>
+  </template>
+  <object class="GtkGestureMultiPress" id="click_gesture">
+    <property name="widget">PhoshOsdWindow</property>
+    <signal name="released" handler="on_button_released" swapped="true"/>
+  </object>
+</interface>

commit 9558bebe023f23f8a423a8afec6ae2747d5ac63c

osd-window: Use G_PARAM_STATIC_STRINGS
Fixes: 12983abe1 ("Add PhoshOsdWindow")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1382>

src/osd-window.c
@@ -165,30 +165,30 @@ phosh_osd_window_class_init (PhoshOsdWindowClass *klass)
                          "Connector",
                          "Connector to use for osd display",
                          NULL,
-                         G_PARAM_READWRITE);
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
   props[PROP_LABEL] =
     g_param_spec_string ("label",
                          "Label",
                          "Label to show on osd",
                          NULL,
-                         G_PARAM_READWRITE);
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
   props[PROP_ICON_NAME] =
     g_param_spec_string ("icon-name",
                          "Icon Name",
                          "Name of icon to use on osd",
                          NULL,
-                         G_PARAM_READWRITE);
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
   props[PROP_LEVEL] =
     g_param_spec_double ("level",
                          "Level",
                          "Level of bar to display on osd",
-                         0.0,
+                         -1.0,
                          G_MAXDOUBLE,
                          0.0,
-                         G_PARAM_READWRITE);
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
 
   props[PROP_MAX_LEVEL] =
     g_param_spec_double ("max-level",
@@ -197,7 +197,7 @@ phosh_osd_window_class_init (PhoshOsdWindowClass *klass)
                          0.0,
                          G_MAXDOUBLE,
                          0.0,
-                         G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY);
+                         G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
 
   g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 