agx/phosh

commit 988240d3a2dc52f4defa192405fb2836dbf68a94

Add power-dialog and manager
To be shown on power button press or by activating the power button in
settings. This is currently used for screenshots, power-off, screen lock
and the upcoming emergency call support.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1224>

src/power-menu-manager.c
@@ -1,0 +1,216 @@
+/*
+ * Copyright (C) 2023 Guido Günther <agx@sigxcpu.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define G_LOG_DOMAIN "phosh-power-menu-manager"
+
+#include "power-menu-manager.h"
+#include "power-menu.h"
+#include "shell.h"
+#include "util.h"
+
+/**
+ * PhoshPowerMenuManager:
+ *
+ * Handles the power button menu
+ *
+ * The interface is responsible to handle the non-ui parts of a
+ * #PhoshPowerDialog.
+ */
+
+typedef struct _PhoshPowerMenuManager {
+  GObject                parent;
+
+  PhoshPowerMenu        *dialog;
+  GSettings             *settings;
+
+  GSimpleActionGroup    *menu_actions;
+} PhoshPowerMenuManager;
+
+G_DEFINE_TYPE (PhoshPowerMenuManager, phosh_power_menu_manager, G_TYPE_OBJECT)
+
+
+static void
+close_menu (PhoshPowerMenuManager *self)
+{
+  g_clear_pointer (&self->dialog, phosh_cp_widget_destroy);
+}
+
+
+static void
+on_power_menu_done (PhoshPowerMenuManager *self)
+{
+  g_return_if_fail (PHOSH_IS_POWER_MENU_MANAGER (self));
+
+  close_menu (self);
+}
+
+
+static void
+on_power_off_activated (GSimpleAction *action, GVariant *param, gpointer data)
+{
+  PhoshPowerMenuManager *self = PHOSH_POWER_MENU_MANAGER (data);
+  PhoshSessionManager *sm = phosh_shell_get_session_manager (phosh_shell_get_default ());
+
+  g_return_if_fail (PHOSH_IS_POWER_MENU_MANAGER (self));
+
+  close_menu (self);
+  phosh_session_manager_shutdown (sm);
+}
+
+
+static void
+on_screen_lock_activated (GSimpleAction *action, GVariant *param, gpointer data)
+{
+  PhoshPowerMenuManager *self = PHOSH_POWER_MENU_MANAGER (data);
+  PhoshShell *shell = phosh_shell_get_default ();
+
+  g_return_if_fail (PHOSH_IS_POWER_MENU_MANAGER (self));
+
+  close_menu (self);
+  phosh_shell_set_locked (shell, TRUE);
+}
+
+
+static void
+on_screenshot_activated (GSimpleAction *action, GVariant *param, gpointer data)
+
+{
+  PhoshPowerMenuManager *self = PHOSH_POWER_MENU_MANAGER (data);
+  PhoshScreenshotManager *manager =
+    phosh_shell_get_screenshot_manager (phosh_shell_get_default ());
+
+  g_return_if_fail (PHOSH_IS_POWER_MENU_MANAGER (self));
+  g_return_if_fail (PHOSH_IS_SCREENSHOT_MANAGER (manager));
+
+  close_menu (self);
+  phosh_screenshot_manager_do_screenshot (manager, NULL, NULL, FALSE);
+}
+
+
+static void
+on_emergency_call_activated (GSimpleAction *action, GVariant *param, gpointer data)
+{
+  PhoshPowerMenuManager *self = PHOSH_POWER_MENU_MANAGER (data);
+
+  g_return_if_fail (PHOSH_IS_POWER_MENU_MANAGER (self));
+
+  close_menu (self);
+  /* TODO: https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/904 */
+  g_warning ("Activating emergency call menu");
+}
+
+
+static void
+on_power_menu_activated (GSimpleAction *action, GVariant *param, gpointer data)
+{
+  PhoshPowerMenuManager *self = PHOSH_POWER_MENU_MANAGER (data);
+
+  if (self->dialog) {
+    on_power_menu_done (self);
+    return;
+  }
+
+  self->dialog = phosh_power_menu_new (NULL);
+  gtk_widget_insert_action_group (GTK_WIDGET (self->dialog), "power-menu",
+                                  G_ACTION_GROUP (self->menu_actions));
+  g_signal_connect_swapped (self->dialog, "done",
+                            G_CALLBACK (on_power_menu_done), self);
+
+  gtk_widget_show (GTK_WIDGET (self->dialog));
+}
+
+
+static void
+on_shell_state_changed (PhoshPowerMenuManager *self, GParamSpec *pspec, PhoshShell *shell)
+{
+  GAction *action;
+  gboolean enable = TRUE;
+  PhoshShellStateFlags state;
+
+  g_return_if_fail (PHOSH_IS_POWER_MENU_MANAGER (self));
+  g_return_if_fail (PHOSH_IS_SHELL (shell));
+
+  state = phosh_shell_get_state (shell);
+  if (state & PHOSH_STATE_LOCKED)
+    enable = FALSE;
+
+  /* TODO: Make them work on the lock screen too */
+  action = g_action_map_lookup_action (G_ACTION_MAP (self->menu_actions), "screen-lock");
+  g_simple_action_set_enabled (G_SIMPLE_ACTION (action), enable);
+  action = g_action_map_lookup_action (G_ACTION_MAP (self->menu_actions), "poweroff");
+  g_simple_action_set_enabled (G_SIMPLE_ACTION (action), enable);
+}
+
+
+static void
+phosh_power_menu_manager_finalize (GObject *object)
+{
+  PhoshPowerMenuManager *self = PHOSH_POWER_MENU_MANAGER (object);
+
+  g_action_map_remove_action (G_ACTION_MAP (phosh_shell_get_default ()), "power.toggle-menu");
+
+  g_clear_object (&self->menu_actions);
+  g_clear_pointer (&self->dialog, phosh_cp_widget_destroy);
+  g_clear_object (&self->settings);
+
+  G_OBJECT_CLASS (phosh_power_menu_manager_parent_class)->finalize (object);
+}
+
+
+static void
+phosh_power_menu_manager_class_init (PhoshPowerMenuManagerClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->finalize = phosh_power_menu_manager_finalize;
+}
+
+
+static GActionEntry menu_entries[] = {
+  { .name = "poweroff", .activate = on_power_off_activated },
+  { .name = "screen-lock", .activate = on_screen_lock_activated },
+  { .name = "screenshot", .activate = on_screenshot_activated },
+  { .name = "emergency-call", .activate = on_emergency_call_activated },
+};
+
+
+static GActionEntry entries[] = {
+  { .name = "power.toggle-menu", .activate = on_power_menu_activated },
+};
+
+
+static void
+phosh_power_menu_manager_init (PhoshPowerMenuManager *self)
+{
+  GAction *action;
+
+  g_action_map_add_action_entries (G_ACTION_MAP (phosh_shell_get_default ()),
+                                   entries,
+                                   G_N_ELEMENTS (entries),
+                                   self);
+
+  self->menu_actions = g_simple_action_group_new ();
+  g_action_map_add_action_entries (G_ACTION_MAP (self->menu_actions),
+                                   menu_entries, G_N_ELEMENTS (menu_entries),
+                                   self);
+  /* TODO: Until emergency call support landed */
+  action = g_action_map_lookup_action (G_ACTION_MAP (self->menu_actions), "emergency-call");
+  g_simple_action_set_enabled (G_SIMPLE_ACTION (action), FALSE);
+
+  g_signal_connect_object (phosh_shell_get_default (),
+                           "notify::shell-state",
+                           G_CALLBACK (on_shell_state_changed),
+                           self,
+                           G_CONNECT_SWAPPED);
+  on_shell_state_changed (self, NULL, phosh_shell_get_default ());
+}
+
+
+PhoshPowerMenuManager *
+phosh_power_menu_manager_new (void)
+{
+  return g_object_new (PHOSH_TYPE_POWER_MENU_MANAGER, NULL);
+}

po/POTFILES.in
@@ -69,6 +69,7 @@ src/ui/notification-content.ui
 src/ui/notification-frame.ui
 src/ui/overview.ui
 src/ui/polkit-auth-prompt.ui
+src/ui/power-menu.ui
 src/ui/quick-setting.ui
 src/ui/run-command-dialog.ui
 src/ui/settings.ui

src/meson.build
@@ -139,6 +139,10 @@ libphosh_tool_sources = files(
   'osd-window.h',
   'plugin-loader.c',
   'plugin-loader.h',
+  'power-menu.c',
+  'power-menu.h',
+  'power-menu-manager.c',
+  'power-menu-manager.h',
   'revealer.c',
   'revealer.h',
   'status-icon.c',

src/phosh.gresources.xml
@@ -17,6 +17,7 @@
     <file preprocess="xml-stripblanks">ui/polkit-auth-prompt.ui</file>
     <file preprocess="xml-stripblanks">ui/network-auth-prompt.ui</file>
     <file preprocess="xml-stripblanks">ui/osd-window.ui</file>
+    <file preprocess="xml-stripblanks">ui/power-menu.ui</file>
     <file preprocess="xml-stripblanks">ui/run-command-dialog.ui</file>
     <file preprocess="xml-stripblanks">ui/settings.ui</file>
     <file preprocess="xml-stripblanks">ui/system-modal-dialog.ui</file>

src/power-menu-manager.h
@@ -1,0 +1,14 @@
+/*
+ * Copyright (C) 2023 Guido Günther <agx@sigxcpu.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+#define PHOSH_TYPE_POWER_MENU_MANAGER (phosh_power_menu_manager_get_type ())
+G_DECLARE_FINAL_TYPE (PhoshPowerMenuManager, phosh_power_menu_manager, PHOSH, POWER_MENU_MANAGER, GObject);
+
+PhoshPowerMenuManager *phosh_power_menu_manager_new (void);

src/power-menu.c
@@ -1,0 +1,75 @@
+/*
+ * Copyright (C) 2023 Guido Günther <agx@sigxcpu.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define G_LOG_DOMAIN "phosh-power-menu"
+
+#include "config.h"
+
+#include "power-menu.h"
+#include "shell.h"
+#include "session-manager.h"
+
+/**
+ * PhoshPowerMenu:
+ *
+ * Menu on power button press
+ */
+enum {
+  DONE,
+  N_SIGNALS
+};
+static guint signals[N_SIGNALS] = {0};
+
+
+struct _PhoshPowerMenu {
+  PhoshSystemModalDialog parent;
+
+  gboolean               unused;
+};
+G_DEFINE_TYPE (PhoshPowerMenu, phosh_power_menu, PHOSH_TYPE_SYSTEM_MODAL_DIALOG)
+
+
+static void
+on_power_menu_done (PhoshPowerMenu *self)
+{
+  g_return_if_fail (PHOSH_IS_POWER_MENU (self));
+  g_signal_emit (self, signals[DONE], 0);
+}
+
+
+static void
+phosh_power_menu_class_init (PhoshPowerMenuClass *klass)
+{
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  signals[DONE] = g_signal_new ("done",
+                                G_TYPE_FROM_CLASS (klass),
+                                G_SIGNAL_RUN_LAST,
+                                0, NULL, NULL, NULL,
+                                G_TYPE_NONE, 0);
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/sm/puri/phosh/ui/power-menu.ui");
+  gtk_widget_class_bind_template_callback (widget_class, on_power_menu_done);
+
+  gtk_widget_class_set_css_name (widget_class, "phosh-power-menu");
+}
+
+
+static void
+phosh_power_menu_init (PhoshPowerMenu *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+
+PhoshPowerMenu *
+phosh_power_menu_new (PhoshMonitor *monitor)
+{
+  return PHOSH_POWER_MENU (g_object_new (PHOSH_TYPE_POWER_MENU,
+                                         "monitor", monitor,
+                                         NULL));
+}

src/power-menu.h
@@ -1,0 +1,21 @@
+/*
+ * Copyright (C) 2023 Guido Günther <agx@sigxcpu.org>
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include "system-modal-dialog.h"
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define PHOSH_TYPE_POWER_MENU (phosh_power_menu_get_type ())
+
+G_DECLARE_FINAL_TYPE (PhoshPowerMenu, phosh_power_menu, PHOSH, POWER_MENU, PhoshSystemModalDialog)
+
+PhoshPowerMenu *phosh_power_menu_new (PhoshMonitor *monitor);
+
+G_END_DECLS

src/shell.c
@@ -53,6 +53,7 @@
 #include "monitor-manager.h"
 #include "monitor/monitor.h"
 #include "mount-manager.h"
+#include "power-menu-manager.h"
 #include "revealer.h"
 #include "settings.h"
 #include "system-modal-dialog.h"
@@ -157,6 +158,7 @@ typedef struct
   PhoshVpnManager *vpn_manager;
   PhoshPortalAccessManager *portal_access_manager;
   PhoshSuspendManager *suspend_manager;
+  PhoshPowerMenuManager *power_menu_manager;
 
   /* sensors */
   PhoshSensorProxyManager *sensor_proxy_manager;
@@ -512,6 +514,7 @@ phosh_shell_dispose (GObject *object)
   g_clear_object (&priv->notification_banner);
 
   /* dispose managers in opposite order of declaration */
+  g_clear_object (&priv->power_menu_manager);
   g_clear_object (&priv->portal_access_manager);
   g_clear_object (&priv->vpn_manager);
   g_clear_object (&priv->network_auth_manager);
@@ -751,6 +754,7 @@ setup_idle_cb (PhoshShell *self)
   priv->network_auth_manager = phosh_network_auth_manager_new ();
   priv->portal_access_manager = phosh_portal_access_manager_new ();
   priv->suspend_manager = phosh_suspend_manager_new ();
+  priv->power_menu_manager = phosh_power_menu_manager_new ();
 
   setup_primary_monitor_signal_handlers (self);
 

commit a3af73e1d4273046c9459791862022bb1ee04c91

power-menu-manager: Remove unused settings variable
Fixes: 988240d3a ("Add power-dialog and manager")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1393>

src/power-menu-manager.c
@@ -24,7 +24,6 @@ typedef struct _PhoshPowerMenuManager {
   GObject                parent;
 
   PhoshPowerMenu        *dialog;
-  GSettings             *settings;
 
   GSimpleActionGroup    *menu_actions;
 } PhoshPowerMenuManager;
@@ -154,7 +153,6 @@ phosh_power_menu_manager_finalize (GObject *object)
 
   g_clear_object (&self->menu_actions);
   g_clear_pointer (&self->dialog, phosh_cp_widget_destroy);
-  g_clear_object (&self->settings);
 
   G_OBJECT_CLASS (phosh_power_menu_manager_parent_class)->finalize (object);
 }