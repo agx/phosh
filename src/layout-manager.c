agx/phosh

commit 474b07f90c2bc80de80292baab8e7c92362b8fdf

layout-manager: Allow to query rounded corner information
Based on the panel height and the icon size provide the shift in pixels
from the screen edge to not occlude an icon.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1388>

src/layout-manager.c
@@ -40,6 +40,7 @@ struct _PhoshLayoutManager {
   GSettings                  *settings;
   PhoshLayoutClockPosition    clock_pos;
   guint                       clock_shift;
+  guint                       corner_shift;
 
   PhoshMonitor               *builtin;
 };
@@ -62,7 +63,6 @@ static GdkRectangle indicators_box_rect = {
   .width = 24 /* px */ * 4 /* max icons */,
   .height = PHOSH_TOP_BAR_HEIGHT
 };
-
 
 /**
  * get_clock_pos:
@@ -152,20 +152,65 @@ get_clock_pos (PhoshLayoutManager *self, guint *clock_shift)
   return clock_pos;
 }
 
+/**
+ * get_corner_shift:
+ * @self: The layout manager
+ *
+ * Get the left and right margins to compensate for rounded corners.
+ *
+ * We assume the panel uses it's native orientation.
+ *
+ * Returns: The margin in pixels
+ */
+static guint
+get_corner_shift (PhoshLayoutManager *self)
+{
+  float r, a, b, c, scale;
+  PhoshShellLayout layout;
+
+  layout = g_settings_get_enum (self->settings, SHELL_LAYOUT_KEY);
+  if (layout != PHOSH_SHELL_LAYOUT_DEVICE)
+    return 0;
+
+  scale = phosh_monitor_get_fractional_scale (self->builtin);
+  r = c = gm_display_panel_get_border_radius (self->panel) / scale;
+
+  /*
+   * We want to keep the n pixels towards the top screen edge clear
+   * n + b = r and m + a = r and c = r
+   *
+   *    ------------------------
+   *    |----+------------------ n
+   *    |    |\
+   *    |  b | \ c = r
+   *    |    |  \
+   *    +----+---*
+   *    | m    a
+   */
+  b = c - (PHOSH_TOP_BAR_HEIGHT - PHOSH_TOP_BAR_ICON_SIZE) / 2;
+  a = floor (sqrt((c * c) - (b * b)));
+
+  return MAX (PHOSH_TOP_BAR_MIN_PADDING, ceil (r - a));
+}
+
 
 static void
 update_layout (PhoshLayoutManager *self)
 {
   PhoshLayoutClockPosition pos;
-  guint shift = 0;
+  guint corner_shift, shift = 0;
 
+  corner_shift = get_corner_shift (self);
   pos = get_clock_pos (self, &shift);
 
-  if (self->clock_pos == pos && self->clock_shift == shift)
+  if (self->corner_shift == corner_shift &&
+      self->clock_pos == pos && self->clock_shift == shift) {
     return;
+  }
 
   self->clock_pos = pos;
   self->clock_shift = shift;
+  self->corner_shift = corner_shift;
 
   g_signal_emit (self, signals[LAYOUT_CHANGED], 0);
 }
@@ -246,6 +291,7 @@ phosh_layout_manager_init (PhoshLayoutManager *self)
   PhoshShell *shell = phosh_shell_get_default ();
   g_autoptr (GmDeviceInfo) info = NULL;
 
+  self->corner_shift = PHOSH_TOP_BAR_MIN_PADDING;
   self->settings = g_settings_new ("sm.puri.phosh");
 
   compatibles = gm_device_tree_get_compatibles (NULL, &err);
@@ -299,4 +345,21 @@ phosh_layout_manager_get_clock_shift (PhoshLayoutManager *self)
   g_return_val_if_fail (PHOSH_IS_LAYOUT_MANAGER (self), 0);
 
   return self->clock_shift;
+}
+
+/**
+ * phosh_layout_manager_get_corner_shift:
+ * @self: The layout manager
+ *
+ * Gets the amount of pixels UI elements should be moved to towards the
+ * center because of rounded corners.
+ *
+ * Returns: Returns the shift in pixels.
+ */
+guint
+phosh_layout_manager_get_corner_shift (PhoshLayoutManager *self)
+{
+  g_return_val_if_fail (PHOSH_IS_LAYOUT_MANAGER (self), 0);
+
+  return self->corner_shift;
 }

src/layout-manager.h
@@ -38,7 +38,6 @@ G_DECLARE_FINAL_TYPE (PhoshLayoutManager, phosh_layout_manager, PHOSH, LAYOUT_MA
 PhoshLayoutManager          *phosh_layout_manager_new (void);
 PhoshLayoutClockPosition     phosh_layout_manager_get_clock_pos        (PhoshLayoutManager *self);
 guint                        phosh_layout_manager_get_clock_shift      (PhoshLayoutManager *self);
-
+guint                        phosh_layout_manager_get_corner_shift     (PhoshLayoutManager *self);
 
 G_END_DECLS
-

commit 5c53008a12d508d125ba7fb58d70a95044a2a60c

layout-manager: Return min padding in non-device mode
If we don't want automatic layout based on device information we should
return the minimal padding rather than 0 as otherwise elements will be
too close to the screen edge.

While at that make it simpler to debug such situations.

Fixes: 474b07f90 ("layout-manager: Allow to query rounded corner information")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1424>

src/layout-manager.c
@@ -166,10 +166,11 @@ get_corner_shift (PhoshLayoutManager *self)
 {
   float r, a, b, c, scale;
   PhoshShellLayout layout;
+  guint shift = PHOSH_TOP_BAR_MIN_PADDING;
 
   layout = g_settings_get_enum (self->settings, SHELL_LAYOUT_KEY);
   if (layout != PHOSH_SHELL_LAYOUT_DEVICE)
-    return 0;
+    goto out;
 
   scale = phosh_monitor_get_fractional_scale (self->builtin);
   r = c = gm_display_panel_get_border_radius (self->panel) / scale;
@@ -189,7 +190,12 @@ get_corner_shift (PhoshLayoutManager *self)
   b = c - (PHOSH_TOP_BAR_HEIGHT - PHOSH_TOP_BAR_ICON_SIZE) / 2;
   a = floor (sqrt((c * c) - (b * b)));
 
-  return MAX (PHOSH_TOP_BAR_MIN_PADDING, ceil (r - a));
+  shift = MAX (PHOSH_TOP_BAR_MIN_PADDING, ceil (r - a));
+
+ out:
+  g_debug ("Corner shift: %u", shift);
+
+  return shift;
 }
 
 