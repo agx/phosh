agx/phosh

commit 1bb5c8c63c312bc207a66cd482e344858be8306c

shell: Allow shell singleton to be overidden
This follows the pattern first established with
phosh_wall_clock_set_default.

This allows libphosh consumers to override the default shell singleton
with a custom subclass.

Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1435>

src/main.c
@@ -96,7 +96,7 @@ int main(int argc, char *argv[])
   GError *err = NULL;
   gboolean unlocked = FALSE, locked = FALSE, version = FALSE;
   g_autoptr(PhoshWayland) wl = NULL;
-  g_autoptr(PhoshShell) shell = NULL;
+  g_autoptr(PhoshShell) shell = phosh_shell_new ();
   g_autoptr (PhoshBackgroundCache) background_cache = NULL;
   g_autoptr (GTimer) timer = g_timer_new ();
   g_autoptr (PhoshWallClock) wall_clock = phosh_wall_clock_new ();
@@ -137,10 +137,10 @@ int main(int argc, char *argv[])
   g_unix_signal_add (SIGINT, on_shutdown_signal, NULL);
   g_unix_signal_add (SIGUSR1, on_sigusr1_signal, NULL);
 
+  phosh_shell_set_default (shell);
   phosh_wall_clock_set_default (wall_clock);
   wl = phosh_wayland_get_default ();
   background_cache = phosh_background_cache_get_default ();
-  shell = phosh_shell_get_default ();
 
   g_signal_connect (shell, "ready", G_CALLBACK (on_shell_ready), timer);
 

src/plugin-shell.h
@@ -34,6 +34,7 @@ struct _PhoshShellClass {
   void (*_phosh_reserved9) (void);
 };
 
+void                       phosh_shell_set_default (PhoshShell *self);
 PhoshShell                *phosh_shell_get_default (void);
 
 /* Created by the shell on startup */

src/shell.c
@@ -1274,6 +1274,12 @@ phosh_shell_init (PhoshShell *self)
 
 /* }}} */
 
+PhoshShell *
+phosh_shell_new (void)
+{
+  return g_object_new (PHOSH_TYPE_SHELL, NULL);
+}
+
 static gboolean
 select_fallback_monitor (gpointer data)
 {
@@ -2049,6 +2055,25 @@ phosh_shell_get_area (PhoshShell *self, int *width, int *height)
     *height = h + PHOSH_TOP_BAR_HEIGHT + PHOSH_HOME_BAR_HEIGHT;
 }
 
+static PhoshShell *instance;
+
+/**
+ * phosh_shell_set_default:
+ * @self: The shell to use
+ *
+ * Set the PhoshShell singleton that is returned by `phosh_shell_get_default()`
+ */
+void
+phosh_shell_set_default (PhoshShell *self)
+{
+  g_return_if_fail (PHOSH_IS_SHELL (self));
+
+  g_clear_object (&instance);
+
+  instance = self;
+  g_object_add_weak_pointer (G_OBJECT (instance), (gpointer *)&instance);
+}
+
 /**
  * phosh_shell_get_default:
  *
@@ -2059,13 +2084,8 @@ phosh_shell_get_area (PhoshShell *self, int *width, int *height)
 PhoshShell *
 phosh_shell_get_default (void)
 {
-  static PhoshShell *instance;
-
-  if (instance == NULL) {
-    g_debug("Creating shell");
-    instance = g_object_new (PHOSH_TYPE_SHELL, NULL);
-    g_object_add_weak_pointer (G_OBJECT (instance), (gpointer *)&instance);
-  }
+  if (!instance)
+    g_error ("Shell singleton not set");
   return instance;
 }
 

src/shell.h
@@ -75,6 +75,7 @@ typedef enum {
 } PhoshShellDebugFlags;
 
 
+PhoshShell          *phosh_shell_new             (void);
 PhoshShellDebugFlags phosh_shell_get_debug_flags (void);
 void                 phosh_shell_get_usable_area (PhoshShell *self,
                                                   int        *x,

tests/test-shell.c
@@ -59,7 +59,7 @@ on_idle (gpointer data)
  * phosh_test_get_shell:
  * @saved_flags: Current logging flags
  *
- * Returns: A new shell object
+ * Returns:(transfer full): A new shell object
  */
 static PhoshShell *
 phosh_test_get_shell (GLogLevelFlags *saved_flags)
@@ -77,7 +77,7 @@ phosh_test_get_shell (GLogLevelFlags *saved_flags)
   flags = g_log_set_always_fatal (0);
   g_log_set_always_fatal(flags & ~G_LOG_LEVEL_WARNING);
 
-  shell = phosh_shell_get_default ();
+  shell = phosh_shell_new ();
   g_assert_true (PHOSH_IS_SHELL (shell));
 
   g_assert_false (phosh_shell_get_locked (shell));
@@ -127,6 +127,8 @@ test_shell_new (PhoshTestCompositorFixture *fixture, gconstpointer unused)
   gboolean ready = FALSE;
 
   shell = phosh_test_get_shell (&flags);
+  phosh_shell_set_default (shell);
+
   g_signal_connect (shell, "ready", G_CALLBACK (on_shell_ready), &ready);
 
   mm = phosh_shell_get_monitor_manager (shell);
@@ -159,6 +161,7 @@ test_shell_new_two_outputs (PhoshTestCompositorFixture *fixture, gconstpointer u
   gboolean success;
 
   shell = phosh_test_get_shell (&flags);
+  phosh_shell_set_default (shell);
 
   mm = phosh_shell_get_monitor_manager (shell);
   g_assert_cmpint (phosh_monitor_manager_get_num_monitors (mm), ==, 2);

tests/testlib-full-shell.c
@@ -46,7 +46,7 @@ kill_compositor (int signum)
 static gpointer
 phosh_test_full_shell_thread (gpointer data)
 {
-  PhoshShell *shell;
+  PhoshShell *shell = NULL;
   GLogLevelFlags flags;
   PhoshTestFullShellFixture *fixture = (PhoshTestFullShellFixture *)data;
   /* Fake the date/time to that of the initial commit to Phosh git repository.
@@ -74,9 +74,9 @@ phosh_test_full_shell_thread (gpointer data)
   flags = g_log_set_always_fatal (0);
   g_log_set_always_fatal (flags & ~G_LOG_LEVEL_WARNING);
 
+  shell = phosh_shell_new ();
+  phosh_shell_set_default (shell);
   phosh_wall_clock_set_default (PHOSH_WALL_CLOCK (wall_clock));
-  shell = phosh_shell_get_default ();
-  g_assert_true (PHOSH_IS_SHELL (shell));
 
   gtk_css_provider_load_from_resource (provider,
                                        "/mobi/phosh/tests/screenshot-no-anim-overrides.css");

commit ead73ace0b67a9e5ac046da2a80b6cbff14e87a9

main: Instantiate shell after the needed objects
The shells object construction needs the Wayland object and the
background manager.

Fixes: 1bb5c8c63 ("shell: Allow shell singleton to be overidden")

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1436>

src/main.c
@@ -96,7 +96,7 @@ int main(int argc, char *argv[])
   GError *err = NULL;
   gboolean unlocked = FALSE, locked = FALSE, version = FALSE;
   g_autoptr(PhoshWayland) wl = NULL;
-  g_autoptr(PhoshShell) shell = phosh_shell_new ();
+  g_autoptr(PhoshShell) shell = NULL;
   g_autoptr (PhoshBackgroundCache) background_cache = NULL;
   g_autoptr (GTimer) timer = g_timer_new ();
   g_autoptr (PhoshWallClock) wall_clock = phosh_wall_clock_new ();
@@ -137,10 +137,11 @@ int main(int argc, char *argv[])
   g_unix_signal_add (SIGINT, on_shutdown_signal, NULL);
   g_unix_signal_add (SIGUSR1, on_sigusr1_signal, NULL);
 
-  phosh_shell_set_default (shell);
   phosh_wall_clock_set_default (wall_clock);
   wl = phosh_wayland_get_default ();
   background_cache = phosh_background_cache_get_default ();
+  shell = phosh_shell_new ();
+  phosh_shell_set_default (shell);
 
   g_signal_connect (shell, "ready", G_CALLBACK (on_shell_ready), timer);
 