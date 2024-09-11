c555ead7b22b3a2e2451c5497b82baf6e66495fb
Guido Günther <agx@sigxcpu.org>
2024-09-11 10:21:01 +0200

    ci: Verify libphosh install
    Compile a small stub to ensure the pkgconf file works
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1523>

---
+++ .gitlab-ci/libphosh-test.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2024 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#include <libphosh.h>
+
+int main(void)
+{
+}
