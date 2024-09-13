834d02b1d4a4bba8bb7cac821be095ffbfab81dd
Gotam Gorabh <gautham@localhost.localdomain>
2024-09-13 08:15:12 +0530

    upcoming-events-prefs: Switch to AdwPreferencesDialog
    This avoids "warning: ‘adw_preferences_window_get_type’ is deprecated:
    Use 'AdwPreferencesDialog' instead" when building against (unreleased) libadwaita 1.6.
    
    See: https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/issues/64
    Depends on: https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/152
    
    Signed-off-by: Gotam Gorabh <gautamy672@gmail.com>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1515>

--- plugins/upcoming-events/prefs/upcoming-events-prefs.h
+++ plugins/upcoming-events/prefs/upcoming-events-prefs.h
@@ -11,6 +11,6 @@
 G_BEGIN_DECLS
 
 #define PHOSH_TYPE_UPCOMING_EVENTS_PREFS (phosh_upcoming_events_prefs_get_type ())
-G_DECLARE_FINAL_TYPE (PhoshUpcomingEventsPrefs, phosh_upcoming_events_prefs, PHOSH, UPCOMING_EVENTS_PREFS, AdwPreferencesWindow)
+G_DECLARE_FINAL_TYPE (PhoshUpcomingEventsPrefs, phosh_upcoming_events_prefs, PHOSH, UPCOMING_EVENTS_PREFS, AdwPreferencesDialog)
 
 G_END_DECLS
