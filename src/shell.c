b1e558a5435b6c5cdeba5af35b098596a8b3de1b
Guido Günther <agx@sigxcpu.org>
2024-11-03 13:53:07 +0100

    shell: Use `once` function for timeout handling
    Simpler and less error prone
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1565>

--- src/shell.c
+++ src/shell.c
@@ -677,18 +677,18 @@ notify_compositor_up_state (PhoshShell *self, enum phosh_private_shell_state sta
 }
 
 
-static gboolean
-on_startup_finished (PhoshShell *self)
+static void
+on_startup_finished (gpointer data)
 {
+  PhoshShell *self = PHOSH_SHELL (data);
   PhoshShellPrivate *priv;
 
-  g_return_val_if_fail (PHOSH_IS_SHELL (self), G_SOURCE_REMOVE);
+  g_return_if_fail (PHOSH_IS_SHELL (self));
   priv = phosh_shell_get_instance_private (self);
 
   notify_compositor_up_state (self, PHOSH_PRIVATE_SHELL_STATE_UP);
 
   priv->startup_finished_id = 0;
-  return G_SOURCE_REMOVE;
 }
 
 
@@ -776,7 +776,7 @@ setup_idle_cb (PhoshShell *self)
 
   /* Delay signaling to the compositor a bit so that idle handlers get a chance to run and
      the user can unlock right away. Ideally we'd not need this */
-  priv->startup_finished_id = g_timeout_add_seconds (1, (GSourceFunc)on_startup_finished, self);
+  priv->startup_finished_id = g_timeout_add_seconds_once (1, on_startup_finished, self);
   g_source_set_name_by_id (priv->startup_finished_id, "[PhoshShell] startup finished");
 
   priv->startup_finished = TRUE;
