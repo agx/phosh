73d20aa51d4bd0aa7e036982602cd51b63737683
Guido Günther <agx@sigxcpu.org>
2025-04-13 11:56:24 +0200

    app-list-model: Expand test
    Assert that we finalize properly and also check the state
    when app infos got loaded.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1700>

--- tests/test-app-list-model.c
+++ tests/test-app-list-model.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2019 Purism SPC
+ *               2025 The Phosh Developers
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
@@ -16,35 +17,73 @@ test_phosh_app_list_model_get_default(void)
 
   g_assert_true (PHOSH_IS_APP_LIST_MODEL (model1));
   g_assert_true (model1 == model2);
-  g_object_unref (model1);
+  g_assert_finalize_object (model1);
 
   /* test we can do that twice in a row */
   model1 = phosh_app_list_model_get_default ();
   g_assert_true (PHOSH_IS_APP_LIST_MODEL (model1));
-  g_object_unref (model1);
+  g_assert_finalize_object (model1);
+}
 
+
+typedef struct {
+  GMainLoop *loop;
+  PhoshAppListModel *model;
+  gboolean changed;
+} ItemsChangedContext;
+
+
+static void
+on_items_changed (GListModel *model,
+                  guint       position,
+                  guint       removed,
+                  guint       added,
+                  gpointer    user_data)
+{
+  ItemsChangedContext *context = user_data;
+
+  g_assert_nonnull (context);
+  g_assert_true (model == G_LIST_MODEL (context->model));
+  g_assert_true (PHOSH_IS_APP_LIST_MODEL (model));
+  g_assert_cmpint (g_list_model_get_n_items (G_LIST_MODEL (model)), >, 0);
+  g_assert_cmpint (g_list_model_get_n_items (G_LIST_MODEL (model)), ==, added);
+  g_assert_cmpint (removed, ==, 0);
+  g_assert_cmpint (position, ==, 0);
+
+  context->changed = TRUE;
+  g_main_loop_quit (context->loop);
 }
 
 
 static void
-test_phosh_app_list_model_g_list_iface (void)
+test_phosh_app_list_model_api (void)
 {
   PhoshAppListModel *model = phosh_app_list_model_get_default ();
+  g_autoptr (GMainLoop) loop = g_main_loop_new (NULL, FALSE);
+  ItemsChangedContext context = {
+    .loop = loop,
+    .model = model,
+  };
 
   g_assert_true (PHOSH_IS_APP_LIST_MODEL (model));
   g_assert_true (G_IS_LIST_MODEL (model));
   g_assert_cmpint (g_list_model_get_n_items (G_LIST_MODEL (model)), ==, 0);
   g_assert_true (g_list_model_get_item_type (G_LIST_MODEL (model)) == G_TYPE_APP_INFO);
+  g_signal_connect (model, "items-changed", G_CALLBACK (on_items_changed), &context);
+  g_main_loop_run (loop);
+
+  g_assert_true (context.changed);
+  g_assert_finalize_object (model);
 }
 
 
 int
-main (int   argc,
-      char *argv[])
+main (int argc, char *argv[])
 {
   g_test_init (&argc, &argv, NULL);
 
   g_test_add_func("/phosh/app-list-model/new", test_phosh_app_list_model_get_default);
-  g_test_add_func("/phosh/app-list-model/g_list_iface", test_phosh_app_list_model_g_list_iface);
+  g_test_add_func("/phosh/app-list-model/api", test_phosh_app_list_model_api);
+
   return g_test_run();
 }
