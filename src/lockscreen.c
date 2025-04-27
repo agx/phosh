a11ffd9f2c7d9cf0306a816c282f2e145b9152d0
Guido Günther <agx@sigxcpu.org>
2025-04-27 18:12:54 +0200

    lockscreen: Simplify auth callback
    Use canonical naming and void cast
    
    Gbp-Dch: Ignore
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1710>

--- src/lockscreen.c
+++ src/lockscreen.c
@@ -289,10 +289,12 @@ focus_pin_entry (PhoshLockscreen *self, gboolean enable_osk)
   gtk_entry_grab_focus_without_selecting (GTK_ENTRY (priv->entry_pin));
 }
 
-/* callback of async auth task */
+
 static void
-auth_async_cb (PhoshAuth *auth, GAsyncResult *result, PhoshLockscreen *self)
+on_auth_authenticate_ready (GObject *source_object, GAsyncResult *result, gpointer user_data)
 {
+  PhoshAuth *auth = PHOSH_AUTH (source_object);
+  PhoshLockscreen *self = PHOSH_LOCKSCREEN (user_data);
   PhoshLockscreenPrivate *priv;
   GError *error = NULL;
   gboolean authenticated;
@@ -979,7 +981,7 @@ on_unlock_submit (PhoshLockscreen *self)
   phosh_auth_authenticate_async (priv->auth,
                                  input,
                                  NULL,
-                                 (GAsyncReadyCallback)auth_async_cb,
+                                 on_auth_authenticate_ready,
                                  g_object_ref (self));
 }
 
