agx/phosh

commit b281cc5d3c59cdf3b04284b4c250051bf3ecd806

cell-broadcast-prompt: New dialog for cell broadcast message
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1334>

src/cell-broadcast-prompt.c
@@ -1,0 +1,170 @@
+/*
+ * Copyright (C) 2024 Guido Günther
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#define G_LOG_DOMAIN "phosh-cell-broadcast-prompt"
+
+#include "phosh-config.h"
+
+#include "cell-broadcast-prompt.h"
+
+#include <glib/gi18n.h>
+
+/**
+ * PhoshCellBroadcastPrompt:
+ *
+ * A modal prompt to display cell broadcast messages
+ *
+ * The #PhoshCellBroadcastPrompt is used to show cell
+ * broacast messages from the mobile network.
+ *
+ * Since it's about emergencies it can be shown above the
+ * lock screen.
+ */
+
+enum {
+  PROP_0,
+  PROP_MESSAGE,
+  PROP_LAST_PROP,
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+
+enum {
+  CLOSED,
+  N_SIGNALS
+};
+static guint signals[N_SIGNALS] = { 0 };
+
+
+struct _PhoshCellBroadcastPrompt {
+  PhoshSystemModalDialog parent;
+
+  GtkLabel              *lbl_msg;
+  char                  *message;
+};
+G_DEFINE_TYPE (PhoshCellBroadcastPrompt, phosh_cell_broadcast_prompt, PHOSH_TYPE_SYSTEM_MODAL_DIALOG);
+
+
+static void
+set_message (PhoshCellBroadcastPrompt *self, const char *message)
+{
+  g_return_if_fail (PHOSH_IS_CELL_BROADCAST_PROMPT (self));
+
+  if (!g_strcmp0 (self->message, message))
+    return;
+
+  g_clear_pointer (&self->message, g_free);
+  self->message = g_strdup (message);
+
+  gtk_label_set_label (self->lbl_msg, message);
+
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_MESSAGE]);
+}
+
+
+static void
+phosh_cell_broadcast_prompt_set_property (GObject      *obj,
+                                          guint         prop_id,
+                                          const GValue *value,
+                                          GParamSpec   *pspec)
+{
+  PhoshCellBroadcastPrompt *self = PHOSH_CELL_BROADCAST_PROMPT (obj);
+
+  switch (prop_id) {
+  case PROP_MESSAGE:
+    set_message (self, g_value_get_string (value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, prop_id, pspec);
+    break;
+  }
+}
+
+
+static void
+phosh_cell_broadcast_prompt_get_property (GObject    *obj,
+                                          guint       prop_id,
+                                          GValue     *value,
+                                          GParamSpec *pspec)
+{
+  PhoshCellBroadcastPrompt *self = PHOSH_CELL_BROADCAST_PROMPT (obj);
+
+  switch (prop_id) {
+  case PROP_MESSAGE:
+    g_value_set_string (value, self->message ?: "");
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (obj, prop_id, pspec);
+    break;
+  }
+}
+
+
+static void
+on_ok_clicked (PhoshCellBroadcastPrompt *self)
+{
+  g_signal_emit (self, signals[CLOSED], 0);
+}
+
+
+static void
+phosh_cell_broadcast_prompt_finalize (GObject *obj)
+{
+  PhoshCellBroadcastPrompt *self = PHOSH_CELL_BROADCAST_PROMPT (obj);
+
+  g_free (self->message);
+
+  G_OBJECT_CLASS (phosh_cell_broadcast_prompt_parent_class)->finalize (obj);
+}
+
+
+static void
+phosh_cell_broadcast_prompt_class_init (PhoshCellBroadcastPromptClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->get_property = phosh_cell_broadcast_prompt_get_property;
+  object_class->set_property = phosh_cell_broadcast_prompt_set_property;
+  object_class->finalize = phosh_cell_broadcast_prompt_finalize;
+
+  props[PROP_MESSAGE] =
+    g_param_spec_string ("message", "", "",
+                         "",
+                         G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_CONSTRUCT_ONLY);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
+
+  signals[CLOSED] = g_signal_new ("closed",
+                                  G_TYPE_FROM_CLASS (klass), G_SIGNAL_RUN_LAST, 0, NULL, NULL,
+                                  NULL, G_TYPE_NONE, 0);
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/mobi/phosh/ui/cell-broadcast-prompt.ui");
+  gtk_widget_class_bind_template_child (widget_class, PhoshCellBroadcastPrompt, lbl_msg);
+  gtk_widget_class_bind_template_callback (widget_class, on_ok_clicked);
+
+  gtk_widget_class_set_css_name (widget_class, "phosh-cell-broadcast-prompt");
+}
+
+
+static void
+phosh_cell_broadcast_prompt_init (PhoshCellBroadcastPrompt *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
+
+
+GtkWidget *
+phosh_cell_broadcast_prompt_new (const char *message, const char *title)
+{
+  return g_object_new (PHOSH_TYPE_CELL_BROADCAST_PROMPT,
+                       "message", message,
+                       "title", title,
+                       NULL);
+}

src/ui/cell-broadcast-prompt.ui
@@ -1,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <requires lib="gtk+" version="3.20"/>
+  <template class="PhoshCellBroadcastPrompt" parent="PhoshSystemModalDialog">
+    <property name="can-focus">False</property>
+    <child type="phosh-dialog-content">
+      <object class="GtkBox">
+        <property name="visible">1</property>
+        <property name="orientation">vertical</property>
+        <child>
+          <object class="GtkLabel" id="lbl_msg">
+            <property name="visible">1</property>
+            <property name="margin-bottom">12</property>
+            <property name="justify">center</property>
+            <property name="wrap">1</property>
+            <property name="wrap-mode">word-char</property>
+          </object>
+        </child>
+      </object>
+    </child>
+    <child type="phosh-dialog-button">
+      <object class="GtkButton">
+        <property name="label" translatable="yes">Ok</property>
+        <property name="visible">1</property>
+        <property name="can-default">1</property>
+        <property name="has-default">1</property>
+        <property name="valign">center</property>
+        <property name="use-underline">1</property>
+        <signal name="clicked" handler="on_ok_clicked" swapped="yes"/>
+        <style>
+          <class name="suggested-action"/>
+        </style>
+      </object>
+    </child>
+  </template>
+</interface>

po/POTFILES.in
@@ -78,6 +78,7 @@ src/ui/app-grid-button.ui
 src/ui/app-grid.ui
 src/ui/audio-settings.ui
 src/ui/bt-status-page.ui
+src/ui/cell-broadcast-prompt.ui
 src/ui/emergency-menu.ui
 src/ui/emergency-contact-row.ui
 src/ui/end-session-dialog.ui

src/cell-broadcast-prompt.h
@@ -1,0 +1,17 @@
+/*
+ * Copyright (C) 2024 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <gtk/gtk.h>
+#include "system-modal-dialog.h"
+
+#define PHOSH_TYPE_CELL_BROADCAST_PROMPT (phosh_cell_broadcast_prompt_get_type ())
+
+G_DECLARE_FINAL_TYPE (PhoshCellBroadcastPrompt, phosh_cell_broadcast_prompt, PHOSH,
+                      CELL_BROADCAST_PROMPT, PhoshSystemModalDialog)
+
+GtkWidget        *phosh_cell_broadcast_prompt_new (const char *message, const char *title);

src/meson.build
@@ -105,6 +105,7 @@ libphosh_tool_headers = files(
   'call.h',
   'calls-manager.h',
   'call-notification.h',
+  'cell-broadcast-prompt.h',
   'clamp.h',
   'connectivity-info.h',
   'drag-surface.h',
@@ -182,6 +183,7 @@ libphosh_tool_sources = files(
   'call.c',
   'calls-manager.c',
   'call-notification.c',
+  'cell-broadcast-prompt.c',
   'clamp.c',
   'connectivity-info.c',
   'drag-surface.c',

src/phosh.gresources.xml
@@ -12,6 +12,7 @@
     <file preprocess="xml-stripblanks">ui/bt-device-row.ui</file>
     <file preprocess="xml-stripblanks">ui/bt-status-page.ui</file>
     <file preprocess="xml-stripblanks">ui/call-notification.ui</file>
+    <file preprocess="xml-stripblanks">ui/cell-broadcast-prompt.ui</file>
     <file preprocess="xml-stripblanks">ui/emergency-contact-row.ui</file>
     <file preprocess="xml-stripblanks">ui/emergency-menu.ui</file>
     <file preprocess="xml-stripblanks">ui/end-session-dialog.ui</file>

commit 17453a8d55b80aa83c09ff6eba67a1408c5f99c9

cell-broadcast-prompt: Close dialog on swipe
Fixes: b281cc5d3 ("cell-broadcast-prompt: New dialog for cell broadcast message")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh/-/merge_requests/1753>

src/cell-broadcast-prompt.c
@@ -113,6 +113,13 @@ on_ok_clicked (PhoshCellBroadcastPrompt *self)
 
 
 static void
+on_dialog_canceled (PhoshCellBroadcastPrompt *self)
+{
+  g_signal_emit (self, signals[CLOSED], 0);
+}
+
+
+static void
 phosh_cell_broadcast_prompt_finalize (GObject *obj)
 {
   PhoshCellBroadcastPrompt *self = PHOSH_CELL_BROADCAST_PROMPT (obj);
@@ -147,6 +154,7 @@ phosh_cell_broadcast_prompt_class_init (PhoshCellBroadcastPromptClass *klass)
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/mobi/phosh/ui/cell-broadcast-prompt.ui");
   gtk_widget_class_bind_template_child (widget_class, PhoshCellBroadcastPrompt, lbl_msg);
+  gtk_widget_class_bind_template_callback (widget_class, on_dialog_canceled);
   gtk_widget_class_bind_template_callback (widget_class, on_ok_clicked);
 
   gtk_widget_class_set_css_name (widget_class, "phosh-cell-broadcast-prompt");

src/ui/cell-broadcast-prompt.ui
@@ -2,6 +2,7 @@
 <interface>
   <requires lib="gtk+" version="3.24"/>
   <template class="PhoshCellBroadcastPrompt" parent="PhoshSystemModalDialog">
+    <signal name="dialog-canceled" handler="on_dialog_canceled"/>
     <child type="phosh-dialog-content">
       <object class="GtkBox">
         <property name="visible">1</property>